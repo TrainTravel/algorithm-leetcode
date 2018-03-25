a39c298584c2c4515e6a0c6c058cf3400354bb9e
Mirage09 <borismirage00@gmail.com>
2018-03-25 16:59:49 -0400

    Minor fix

--- src/LeetCodeSolution/TwoSum.java
+++ src/LeetCodeSolution/TwoSum.java
@@ -1,5 +1,6 @@
 package LeetCodeSolution;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -11,8 +12,8 @@ class TwoSum {
         HashMap<Integer, Integer> arrayMap = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (arrayMap.containsKey(target - nums[i])){
-                return new int[] {i, arrayMap.get(target - nums[i])};
+            if (arrayMap.containsKey(target - nums[i])) {
+                return new int[]{i, arrayMap.get(target - nums[i])};
             }
             arrayMap.put(nums[i], i);
         }
@@ -21,5 +22,9 @@ class TwoSum {
     }
 
     public static void main(String[] args) {
+        TwoSum test = new TwoSum();
+        int[] testArray = {3, 5, 6, 8, 7};
+        int num = 8;
+        System.out.println(Arrays.toString(test.twoSum(testArray, num)));
     }
 }
