7526ffd5b6453f948fe7bc2b45ca21889946b28e
Mirage09 <borismirage00@gmail.com>
2018-03-25 12:25:25 -0400

    Init

--- src/LeetCodeSolution/FindMedianSortedArrays.java
+++ src/LeetCodeSolution/FindMedianSortedArrays.java
@@ -13,9 +13,9 @@ class FindMedianSortedArrays {
      * The object is to find the median of the two sorted arrays.
      * The overall run time complexity should be O(log (m+n)).
      *
-     * @param nums1 first 
-     * @param nums2
-     * @return
+     * @param nums1 first sorted array with length m
+     * @param nums2 second sorted array with length n
+     * @return median of the two sorted arrays
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
