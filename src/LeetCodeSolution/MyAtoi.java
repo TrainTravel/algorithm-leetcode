fd2b7264c9cdeacc86ccd35934b3147fe757d0cf
Mirage09 <borismirage00@gmail.com>
2018-03-26 17:15:20 -0400

    Accepted version
    Although this version is accepted, it is still too slow.
    
    But since this problem IS FUCKING NONSENSE, I will NOT IMPROVE it.

--- src/LeetCodeSolution/MyAtoi.java
+++ src/LeetCodeSolution/MyAtoi.java
@@ -1,5 +1,8 @@
 package LeetCodeSolution;
 
+import java.util.LinkedList;
+
+
 /**
  * Created with IntelliJ IDEA
  * Author: BorisMirage
@@ -8,7 +11,83 @@ package LeetCodeSolution;
  */
 
 public class MyAtoi {
-    public int myAtoi(String str){
-        
+
+    /**
+     * Convert string to integer
+     *
+     * @param str input string
+     * @return integer that is converted
+     */
+
+    public int myAtoi(String str) {
+        LinkedList<Integer> parseList = new LinkedList<>();
+        long result = 0;
+        int power = 0;
+        int cache;
+        int plus = 0;
+        int minus = 0;
+        int validStart = 0;
+
+        if (str.length() == 0) {
+            return 0;
+        }
+
+        /* Check legitimacy */
+        for (int i = 0; i < str.length(); i++) {
+
+            /* ASCII code stands for integer: 48-57 (included) */
+            if ((int) str.charAt(i) > 47 && (int) str.charAt(i) < 58) {
+                parseList.add((int) str.charAt(i) - 48);
+                validStart = 1;
+            } else if ((int) str.charAt(i) == 43) {
+
+                /* 43: "+" */
+                plus += 1;
+                validStart = 1;
+            } else if ((int) str.charAt(i) == 45) {
+                /* 45: "-" */
+                minus += 1;
+                validStart = 1;
+            }
+
+            if (plus + minus > 1) {
+                return 0;
+            }
+            if ((int) str.charAt(i) > 58 || (int) str.charAt(i) < 47) {
+                if ((int) str.charAt(i) != 43 && (int) str.charAt(i) != 45) {
+                    if ((int) str.charAt(i) == 32) {
+                        if (validStart == 1) {
+                            break;
+                        }
+                    } else break;
+
+                }
+            }
+        }
+
+        if (parseList.size() == 0) {
+            return 0;
+        }
+
+        /* Calculate result */
+        while (parseList.size() != 0) {
+            cache = parseList.getLast();
+            result = (long) (cache * Math.pow(10, power) + result);
+            power += 1;
+            parseList.removeLast();
+        }
+
+        if (minus > 0) {
+            result *= -1;
+        }
+
+        if (result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if (result < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        return (int) result;
     }
 }
