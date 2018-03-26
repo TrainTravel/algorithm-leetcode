70d9f9910a8aa5cc31382201b7de0c3d831a4d77
Mirage09 <borismirage00@gmail.com>
2018-03-26 13:38:46 -0400

    Init commit

---
+++ src/LeetCodeSolution/LongestPalindromicSubstring.java
@@ -0,0 +1,14 @@
+package LeetCodeSolution;
+
+/**
+ * Created with IntelliJ IDEA
+ * Author: BorisMirage
+ * Date: 3/26/18
+ * Time: 13:37
+ */
+
+class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        return s;
+    }
+}
