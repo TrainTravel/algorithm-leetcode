0c951188a1b99d72c159e561b5bbc92de108649a
Mirage09 <borismirage00@gmail.com>
2018-03-26 15:30:51 -0400

    Init commit

---
+++ src/LeetCodeSolution/MyAtoi.java
@@ -0,0 +1,14 @@
+package LeetCodeSolution;
+
+/**
+ * Created with IntelliJ IDEA
+ * Author: BorisMirage
+ * Date: 3/26/18
+ * Time: 15:28
+ */
+
+public class MyAtoi {
+    public int myAtoi(String str){
+        
+    }
+}
