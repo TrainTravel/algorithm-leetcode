e00f69ab0660e1efc07272e0c3d388aa75c9cc62
Mirage09 <borismirage00@gmail.com>
2018-03-26 11:38:38 -0400

    Edit test case

--- src/LeetCodeSolution/Test.java
+++ src/LeetCodeSolution/Test.java
@@ -1,7 +1,5 @@
 package LeetCodeSolution;
 
-import java.util.Arrays;
-
 /**
  * Created with IntelliJ IDEA
  * Author: BorisMirage
@@ -10,20 +8,34 @@ import java.util.Arrays;
  */
 
 public class Test {
+
+    public void Test() {
+
+    }
+
     public static void main(String[] args) {
 
-        /* Two Sum test */
-        TwoSum twoSumTest = new TwoSum();
-        int[] testArray = {3, 5, 6, 8, 7};
-        int num = 8;
-        System.out.println(Arrays.toString(twoSumTest.twoSum(testArray, num)));
+//        /* Two Sum test */
+//        TwoSum twoSumTest = new TwoSum();
+//        int[] testArray = {3, 5, 6, 8, 7};
+//        int num = 8;
+//        System.out.println(Arrays.toString(twoSumTest.twoSum(testArray, num)));
+//
+//        /* Longest substring test */
+//        LengthOfLongestSubstring lengthOfLongestSubstringTest = new LengthOfLongestSubstring();
+//        System.out.println(lengthOfLongestSubstringTest.lengthOfLongestSubstring("tmmzuxt"));
+//
+//        /* Add 2 numbers test */
+//        AddTwoNumbers addTwoNumbersTest = new AddTwoNumbers();
 
-        /* Longest substring test */
-        LengthOfLongestSubstring lengthOfLongestSubstringTest = new LengthOfLongestSubstring();
-        System.out.println(lengthOfLongestSubstringTest.lengthOfLongestSubstring("tmmzuxt"));
+        /* Find median test */
+        int[] num1 = {1, 1};
+        int[] num2 = {1, 2};
 
-        /* Add 2 numbers test */
-        AddTwoNumbers addTwoNumbersTest = new AddTwoNumbers();
-        
+        FindMedianSortedArrays findMedianSortedArraysTest = new FindMedianSortedArrays();
+        System.out.println(findMedianSortedArraysTest.findMedianSortedArrays(num1, num2));
+
     }
+
+
 }
