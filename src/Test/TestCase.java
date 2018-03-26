9688ca7c94ddb257e36bb8933a0043aec8612fc1
Mirage09 <borismirage00@gmail.com>
2018-03-26 14:45:56 -0400

    Add independent test class

---
+++ src/Test/TestCase.java
@@ -0,0 +1,54 @@
+package Test;
+
+import java.util.Arrays;
+import Helper.MergeTwoSortedArray;
+import LeetCodeSolution.*;
+
+/**
+ * Created with IntelliJ IDEA
+ * Author: BorisMirage
+ * Date: 3/26/18
+ * Time: 14:40
+ */
+
+public class TestCase {
+    public void testCase() {
+
+    }
+
+    public static void main(String[] args) {
+
+        /* Two Sum Test */
+
+//        TwoSum twoSumTest = new TwoSum();
+//        int[] testArray = {3, 5, 6, 8, 7};
+//        int num = 8;
+//        System.out.println(Arrays.toString(twoSumTest.twoSum(testArray, num)));
+
+//        /* Longest substring Test */
+//        LengthOfLongestSubstring lengthOfLongestSubstringTest = new LengthOfLongestSubstring();
+//        System.out.println(lengthOfLongestSubstringTest.lengthOfLongestSubstring("tmmzuxt"));
+
+//        /* Add 2 numbers Test */
+//        AddTwoNumbers addTwoNumbersTest = new AddTwoNumbers();
+
+//        /* Find median Test */
+//        int[] num1 = {1, 2, 3};
+//        int[] num2 = {4, 5, 6};
+//        FindMedianSortedArrays findMedianSortedArraysTest = new FindMedianSortedArrays();
+//        System.out.println(findMedianSortedArraysTest.findMedianSortedArrays(num1, num2));
+
+//        /* Reverse int Test */
+//        ReverseInteger reverseIntegerTest = new ReverseInteger();
+//        System.out.println(reverseIntegerTest.reverseInteger(1534236469));
+
+//        /* Merge 2 arrays test */
+//        MergeTwoSortedArray mergeTwoSortedArray = new MergeTwoSortedArray();
+//        System.out.println(Arrays.toString(mergeTwoSortedArray.MergeTwoSortedArray(num1, num2)));
+
+//        /* Longest Palindromic Substring Test*/
+//        LongestPalindromicSubstring longestPalindromicSubstringTest = new LongestPalindromicSubstring();
+//        System.out.println(longestPalindromicSubstringTest.longestPalindrome("s"));
+
+    }
+}
