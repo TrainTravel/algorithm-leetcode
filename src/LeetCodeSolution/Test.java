426b87494b917dcddbce4dba1fd2d290db62e182
Mirage09 <borismirage00@gmail.com>
2018-03-26 14:39:34 -0400

    Add test case

--- src/LeetCodeSolution/Test.java
+++ src/LeetCodeSolution/Test.java
@@ -1,5 +1,9 @@
 package LeetCodeSolution;
 
+import Helper.MergeTwoSortedArray;
+
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA
  * Author: BorisMirage
@@ -15,32 +19,36 @@ public class Test {
 
     public static void main(String[] args) {
 
-//        /* Two Sum test */
+//        /* Two Sum Test */
 //        TwoSum twoSumTest = new TwoSum();
 //        int[] testArray = {3, 5, 6, 8, 7};
 //        int num = 8;
 //        System.out.println(Arrays.toString(twoSumTest.twoSum(testArray, num)));
 
-//        /* Longest substring test */
+//        /* Longest substring Test */
 //        LengthOfLongestSubstring lengthOfLongestSubstringTest = new LengthOfLongestSubstring();
 //        System.out.println(lengthOfLongestSubstringTest.lengthOfLongestSubstring("tmmzuxt"));
 
-//        /* Add 2 numbers test */
+//        /* Add 2 numbers Test */
 //        AddTwoNumbers addTwoNumbersTest = new AddTwoNumbers();
 
-//        /* Find median test */
-//        int[] num1 = {};
-//        int[] num2 = {2, 3};
+//        /* Find median Test */
+//        int[] num1 = {1, 2, 3};
+//        int[] num2 = {4, 5, 6};
 //        FindMedianSortedArrays findMedianSortedArraysTest = new FindMedianSortedArrays();
 //        System.out.println(findMedianSortedArraysTest.findMedianSortedArrays(num1, num2));
 
-//        /* Reverse int test */
+//        /* Reverse int Test */
 //        ReverseInteger reverseIntegerTest = new ReverseInteger();
 //        System.out.println(reverseIntegerTest.reverseInteger(1534236469));
 
-        /* Longest Palindromic Substring Test*/
-        LongestPalindromicSubstring longestPalindromicSubstringTest = new LongestPalindromicSubstring();
-        System.out.println(longestPalindromicSubstringTest.longestPalindrome("s"));
+          /* Merge 2 arrays test */
+//        MergeTwoSortedArray mergeTwoSortedArray = new MergeTwoSortedArray();
+//        System.out.println(Arrays.toString(mergeTwoSortedArray.MergeTwoSortedArray(num1, num2)));
+
+//        /* Longest Palindromic Substring Test*/
+//        LongestPalindromicSubstring longestPalindromicSubstringTest = new LongestPalindromicSubstring();
+//        System.out.println(longestPalindromicSubstringTest.longestPalindrome("s"));
 
     }
 
