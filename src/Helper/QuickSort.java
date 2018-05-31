b066beda836d2b8d5f3fa2d9da6e2e0ae4d033b7
Mirage09 <borismirage00@gmail.com>
2018-05-31 14:37:23 -0400

    Init

--- src/Helper/QuickSort.java
+++ src/Helper/QuickSort.java
@@ -8,4 +8,26 @@ package Helper;
  */
 
 public class QuickSort {
+    private int[] toSort;
+    private int pivot;
+
+    public QuickSort(int[] sortArray) {
+        toSort = sortArray;
+    }
+
+    public int[] sort() {
+
+        /* Sort from large to small */
+        pivot = toSort[toSort.length / 2];
+        int i = 0;
+        int j = toSort.length - 1;
+        while (i < toSort.length - 1) {
+            if (toSort[i] < toSort[j]) {
+                i++;
+                j--;
+            }
+            
+        }
+        return toSort;
+    }
 }
