e08c33d9883b54e4c9c8cf6832b6310e044ad1a0
Mirage09 <borismirage00@gmail.com>
2018-05-31 17:14:27 -0400

    Little progress

--- src/Helper/QuickSort.java
+++ src/Helper/QuickSort.java
@@ -9,25 +9,63 @@ package Helper;
 
 public class QuickSort {
     private int[] toSort;
-    private int pivot;
 
     public QuickSort(int[] sortArray) {
         toSort = sortArray;
     }
 
-    public int[] sort() {
+    /**
+     * Quick sort with recursively partition. Sort target array from small to large.
+     *
+     * @param left  left bound INDEX
+     * @param right right bound INDEX
+     * @return partitioned array
+     */
+    public int[] sort(int left, int right) {
 
-        /* Sort from large to small */
-        pivot = toSort[toSort.length / 2];
-        int i = 0;
-        int j = toSort.length - 1;
-        while (i < toSort.length - 1) {
-            if (toSort[i] < toSort[j]) {
-                i++;
-                j--;
-            }
-            
+        if (toSort.length == 1) {
+            return toSort;
         }
+        if (toSort.length == 2) {
+            if (toSort[0] == toSort[1]) {
+                return toSort;
+            } else if (toSort[0] > toSort[1]) {
+                switchElement(0, 1);
+            }
+            return toSort;
+        }
+
+        /* Select pivot and index. Switch pivot and first element in array */
+        int pivotIndex = toSort[(right - left) / 2];
+        switchElement(left, pivotIndex);
+        int i = left;
+        int j = right;
+
+        /* Move pivot, if element next to pivot smaller than pivot, then swith them */
+        while (i < j) {
+            if (toSort[i] < toSort[i + 1]) {
+                switchElement(i, i + 1);
+                i++;
+            } else {
+                /* Pick element from right to left that is smaller than pivot and switch */
+                while (j > i) {
+                    if (toSort[j] > toSort[i + 1]) {
+                        j--;
+                    } else {
+                        switchElement(i + 1, j);
+                        break;
+                    }
+                }
+            }
+        }
+        sort(left, i);
+        sort(j, right);
         return toSort;
+    }
+
+    private void switchElement(int index1, int index2) {
+        toSort[index1] = toSort[index1] + toSort[index2];
+        toSort[index2] = toSort[index1] - toSort[index2];
+        toSort[index1] = toSort[index1] - toSort[index2];
     }
 }
