6652289ab6930533b035bb437b17ced01de92974
Mirage09 <borismirage00@gmail.com>
2018-06-07 20:30:32 -0400

    Minor edit

--- src/LeetCodeSolution/ThreeSum.java
+++ src/LeetCodeSolution/ThreeSum.java
@@ -15,12 +15,12 @@ public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> threeSumResult = new LinkedList<>();
 
-        /* Special Case */
+        /* If array contains less that 3 elements, directly return empty list */
         if (nums.length < 3) {
             return threeSumResult;
         }
 
-        /* Sort and check */
+        /* If sorted array's first element > 0 or last element < 0, directly return empty list */
         Arrays.sort(nums);
         if (nums[0] > 0 || nums[nums.length - 1] < 0) {
             return threeSumResult;
@@ -32,23 +32,30 @@ public class ThreeSum {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
+
             int target = -nums[i];
+
+            /* Include all possible elements (i + 1 to last) */
             int leftIndex = i + 1;
             int rightIndex = nums.length - 1;
 
             while (leftIndex < rightIndex) {
+
+                /* If sum > target, narrow left index (increase sum); otherwise, narrow right index */
                 if (nums[leftIndex] + nums[rightIndex] < target) {
                     leftIndex++;
                 } else if (nums[leftIndex] + nums[rightIndex] > target) {
                     rightIndex--;
                 } else {
 
-                    /* Find one element */
+                    /* Find one */
                     List<Integer> findList = new LinkedList<>();
                     findList.add(nums[i]);
                     findList.add(nums[leftIndex]);
                     findList.add(nums[rightIndex]);
                     threeSumResult.add(findList);
+
+                    /* Keep finding 3sum in this round */
                     leftIndex++;
                     rightIndex--;
 
