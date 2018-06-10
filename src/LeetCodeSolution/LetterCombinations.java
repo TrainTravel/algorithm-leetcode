95adda1f76dabafc09073f3f9de1359d54f31bda
Mirage09 <borismirage00@gmail.com>
2018-06-10 17:42:51 -0400

    Letter Combinations of a Phone Number

--- src/LeetCodeSolution/LetterCombinations.java
+++ src/LeetCodeSolution/LetterCombinations.java
@@ -1,6 +1,6 @@
 package LeetCodeSolution;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,27 +11,55 @@ import java.util.List;
  */
 
 public class LetterCombinations {
+    /**
+     * Given a string containing digits from 2-9 inclusive.
+     * Return all possible letter combinations that the number could represent.
+     * The combination has same length as the digit length
+     *
+     * @param digits input digits
+     * @return string array list that contains all possible combinations
+     */
     public List<String> letterCombinations(String digits) {
-        List<String> combinationResult = new LinkedList<>();
-        char[] digitArray = digits.toCharArray();
-        if (digitArray.length == 0) {
-            return combinationResult;
+        ArrayList<String> result = new ArrayList<String>();
+
+        if (digits.equals("")) {
+            return result;
         }
 
-        /* Map of digit to char */
-        String[] two = {"a", "b", "c"};
-        String[] three = {"d", "e", "f"};
-        String[] four = {"g", "h", "i"};
-        String[] five = {"j", "k", "l"};
-        String[] six = {"m", "n", "o"};
-        String[] seven = {"p", "q", "r", "s"};
-        String[] eight = {"t", "u", "v"};
-        String[] nine = {"w", "x", "y", "z"};
+        /* First time calling function, the combination string is empty. */
+        findCombination(digits, result, "");
 
-        for (int i = 0; i < digitArray.length; i++) {
-            
+        return result;
+    }
+
+    /**
+     * Recursively find all combinations of digit letters.
+     *
+     * @param digit  input digit
+     * @param result each step's result
+     * @param s      each step's combination string
+     */
+    public void findCombination(String digit, ArrayList<String> result, String s) {
+
+        /* Each key's representation */
+        String[] keyboardMap = new String[]{" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        /* Ending point for this recursion */
+        if (s.length() == digit.length()) {
+            result.add(s);
+            return;
         }
 
-        return combinationResult;
+        /* Current string array item that is represented to the digit
+         *  char - '0': to convert char digit to int digit, therefore to find letters in key array */
+        String digitChar = keyboardMap[digit.charAt(s.length()) - '0'];
+
+        /* Traverse every char in key array item */
+        for (int i = 0; i < digitChar.length(); i++) {
+
+            /* Each recursion will include one more char from key item,
+               until combination length is equal to digit length */
+            findCombination(digit, result, s + digitChar.charAt(i));
+        }
     }
 }
