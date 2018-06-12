5e236cde4eea0b45a82813eae905fad1e70e75a1
Mirage09 <borismirage00@gmail.com>
2018-06-12 19:28:45 -0400

    Minor edit

--- src/LeetCodeSolution/MergeTwoLists.java
+++ src/LeetCodeSolution/MergeTwoLists.java
@@ -12,20 +12,24 @@ public class MergeTwoLists {
     /**
      * Merge two sorted linked lists and return it as a new list.
      * The new list should be made by splicing together the nodes of the first two lists.
+     * <p>
+     * Simply compare value in two ListNode. Point smaller one as next node.
      *
-     * Simply
-     *
-     * @param l1
-     * @param l2
-     * @return
+     * @param l1 first node
+     * @param l2 second node
+     * @return next smaller node
      */
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
-        if(l1.val < l2.val){
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
-        } else{
+        } else {
             l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
