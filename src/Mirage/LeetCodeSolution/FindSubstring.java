4c1c1632f460d8c507cb18dff5de4577fbb0014c
Mirage09 <borismirage00@gmail.com>
2018-06-21 11:07:34 -0400

    Minor edit

--- src/Mirage/LeetCodeSolution/FindSubstring.java
+++ src/Mirage/LeetCodeSolution/FindSubstring.java
@@ -66,32 +66,6 @@ public class FindSubstring {
         }
         return res;
 
-//        final Map<String, Integer> wordsMap = new HashMap<>();
-//        for (final String word : words) {
-//            wordsMap.put(word, wordsMap.getOrDefault(word, 0) + 1);
-//        }
-//        final List<Integer> indexes = new ArrayList<>();
-//        final int n = s.length(), num = words.length, len = words[0].length();
-//        for (int i = 0; i < n - num * len + 1; i++) {
-//            final Map<String, Integer> seen = new HashMap<>();
-//            int j = 0;
-//            while (j < num) {
-//                final String word = s.substring(i + j * len, i + (j + 1) * len);
-//                if (wordsMap.containsKey(word)) {
-//                    seen.put(word, seen.getOrDefault(word, 0) + 1);
-//                    if (seen.get(word) > wordsMap.getOrDefault(word, 0)) {
-//                        break;
-//                    }
-//                } else {
-//                    break;
-//                }
-//                j++;
-//            }
-//            if (j == num) {
-//                indexes.add(i);
-//            }
-//        }
-//        return indexes;
 
     }
 }
