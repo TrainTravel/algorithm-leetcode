472a96b431b6b4ed2cd11d2a6f7cd4544f70cf06
Mirage09 <borismirage00@gmail.com>
2018-06-22 13:03:10 -0400

    Sudoku Solver Init Commit

---
+++ src/Mirage/LeetCodeSolution/SolveSudoku.java
@@ -0,0 +1,27 @@
+package Mirage.LeetCodeSolution;
+
+/**
+ * Created with IntelliJ IDEA
+ * Author: BorisMirage
+ * Date: 6/22/18
+ * Time: 13:01
+ */
+
+public class SolveSudoku {
+    /**
+     * Solve a Sudoku puzzle by filling the empty cells.
+     * A sudoku solution must satisfy all of the following rules:
+     * Each of the digits 1-9 must occur exactly once in each row.
+     * Each of the digits 1-9 must occur exactly once in each column.
+     * Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+     * Empty cells are indicated by the character '.'.
+     *
+     * Use backtracking.
+     *
+     * @param board input Sudoku board
+     */
+    public void solveSudoku(char[][] board) {
+        
+
+    }
+}
