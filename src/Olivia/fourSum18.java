fcd404b500646c9dc67d6d928af1facdce69c5d6
OliviaZhangy <Olivia_zhangy@outlook.com>
2018-06-27 16:22:44 -0700

    Update .gitnore

--- src/Olivia/fourSum18.java
+++ src/Olivia/fourSum18.java
@@ -1,9 +1,27 @@
 package src.Olivia;
 
-import java.util.List;
+import java.util.*;
 
 public class fourSum18 {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> cur = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i == 0 || i > 0 && nums[i] != nums[i - 1]) {
+                for (int j = 0; j < nums.length - 2; j++)
+                    if ((j == i + 1) || (j > i + 1) && (nums[j] != nums[j - 1])) {
+                        int startLoc = j + 1;
+                        int endLoc = nums.length - 1;
+                        while (startLoc < endLoc) {
+                            int now = nums[i] + nums[j] + nums[startLoc] + nums[endLoc];
+                            if (now == target) {
+                                List<Integer> part = new ArrayList<>();
+                            }
+                        }
+                    }
+            }
+        }
+
 
     }
 }
