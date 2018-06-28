3ad6a34255ee0938eb8d14ce47b892c4bde96678
Mirage09 <borismirage00@gmail.com>
2018-06-28 13:15:27 -0400

    Jump Game II

---
+++ src/Mirage/LeetCodeSolution/Jump.java
@@ -0,0 +1,59 @@
+package Mirage.LeetCodeSolution;
+
+/**
+ * Created with IntelliJ IDEA
+ * Author: BorisMirage
+ * Date: 6/28/18
+ * Time: 10:40
+ */
+
+public class Jump {
+    /**
+     * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+     * Each element in the array represents your maximum jump length at that position.
+     * i.e., steps can be taken to move forward <= nums[i].
+     * Your goal is to reach the last index in the minimum number of jumps.
+     * <p>
+     * Traverse all possible move distance in each step, find max move distance in each distance. (greedy algorithm)
+     *
+     * @param nums input int array
+     * @return minimum number of jumps
+     */
+    public int jump(int[] nums) {
+
+        /* Special Case */
+        if (nums.length < 2) {
+            return 0;
+        }
+
+        int minSteps = 0;
+
+        /* Start position in each step */
+        int startIndex = 0;
+
+        while (startIndex < nums.length) {
+
+            /* Set max move as maximum jump length at start position */
+            int maxMove = startIndex + nums[startIndex];
+            int nextStart = startIndex;
+
+            /* If one step can move to the end of array, return this step */
+            if (maxMove > nums.length - 2) {
+                return minSteps + 1;
+            }
+
+            /* Find max move distance based on current position */
+            for (int i = startIndex + 1; i < nums.length && i <= nums[startIndex] + startIndex; i++) {
+
+                /* Find longer moving distance */
+                if (maxMove < nums[i] + i) {
+                    maxMove = nums[i] + i;
+                    nextStart = i;
+                }
+            }
+            startIndex = nextStart;
+            minSteps++;
+        }
+        return minSteps;
+    }
+}
--- src/Mirage/Test/SolutionTest.java
+++ src/Mirage/Test/SolutionTest.java
@@ -265,10 +265,24 @@ public class SolutionTest {
 //
 //        System.out.println(groupAnagramsTest.groupAnagrams(strs));
 
-        /* Trapping Rain Water */
-        Trap trapTest = new Trap();
-        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        System.out.println(trapTest.trap(height));
+//        /* Trapping Rain Water */
+//        Trap trapTest = new Trap();
+//        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+//        System.out.println(trapTest.trap(height));
 
+        /* Jump Game II */
+        Jump jumpTest = new Jump();
+        int[] jump = {2, 3, 1, 1, 1};
+        System.out.println("Min step: " + jumpTest.jump(jump));
+        jump = new int[]{1, 3, 2};
+        System.out.println("Min step: " + jumpTest.jump(jump));
+        jump = new int[]{2, 3, 1, 1, 4};
+        System.out.println("Min step: " + jumpTest.jump(jump));
+        jump = new int[]{1, 1, 1, 1};
+        System.out.println("Min step: " + jumpTest.jump(jump));
+        jump = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0};
+        System.out.println("Min step: " + jumpTest.jump(jump));
+        jump = new int[]{4, 1, 1, 3, 1, 1, 1};
+        System.out.println("Min step: " + jumpTest.jump(jump));
     }
 }
