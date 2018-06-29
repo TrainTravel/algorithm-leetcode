cc520bdd23335af99a93f7d02bba53794c4e615e
Mirage09 <borismirage00@gmail.com>
2018-06-29 09:23:20 -0400

    Minor edit

--- src/Olivia/LetterCombinations17.java
+++ src/Olivia/LetterCombinations17.java
@@ -3,22 +3,23 @@ package Olivia;
 import java.util.ArrayList;
 import java.util.List;
 
-public class letterCombinations17 {
+public class LetterCombinations17 {
     public List<String> letterCombinations(String digits) {
         ArrayList<String> result = new ArrayList<>();
-        if(digits.length()==0) return result;
+        if (digits.length() == 0) return result;
         findCombination(result, digits, "");
         return result;
     }
-    private void findCombination(List<String> result, String input, String append){
+
+    private void findCombination(List<String> result, String input, String append) {
         String[] keyboardMap = new String[]{" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         if (append.length() == input.length()) {
             result.add(append);
             return;
         }
-        String cur = keyboardMap[input.charAt(append.length())-'0'];
+        String cur = keyboardMap[input.charAt(append.length()) - '0'];
         for (int i = 0; i < cur.length(); i++) {
-            findCombination(result,input, append + cur.charAt(i));
+            findCombination(result, input, append + cur.charAt(i));
         }
     }
 }
--- src/Olivia/ThreeSumClosest16.java
+++ src/Olivia/ThreeSumClosest16.java
@@ -1,22 +1,22 @@
 package Olivia;
+
 import java.util.Arrays;
 
 public class ThreeSumClosest16 {
     public int threeSumClosest(int[] nums, int target) {
-        int cur = nums[0]+nums[1]+nums[nums.length-1];
+        int cur = nums[0] + nums[1] + nums[nums.length - 1];
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-2;i++){
-            int startLoc = i+1;
-            int endLoc = nums.length-1;
-            while(startLoc<endLoc){
+        for (int i = 0; i < nums.length - 2; i++) {
+            int startLoc = i + 1;
+            int endLoc = nums.length - 1;
+            while (startLoc < endLoc) {
                 int now = nums[i] + nums[startLoc] + nums[endLoc];
-                if(now>target){
+                if (now > target) {
                     endLoc--;
-                }
-                else {
+                } else {
                     startLoc++;
                 }
-                if(Math.abs(now-target)<Math.abs(cur-target)){
+                if (Math.abs(now - target) < Math.abs(cur - target)) {
                     cur = now;
                 }
             }
