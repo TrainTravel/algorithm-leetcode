9afe797c58bf2e2cb3ada88f469b60dcf14b1428
Mirage09 <borismirage00@gmail.com>
2018-06-30 10:05:27 -0400

    Minor edit

--- src/Olivia/LengthOfLongestSubstring.java
+++ src/Olivia/LengthOfLongestSubstring.java
@@ -11,14 +11,14 @@ class LengthOfLongestSubstring {
         if (s == null) {
             return 0;
         }
-        HashMap<Character,Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         int curMax = 0;
-        for(int i =0,j=0;i<s.length();i++){
-            if(map.containsKey(s.charAt(i))){
-                j = Math.max(j,map.get(s.charAt(i))+1);
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
             }
-            map.put(s.charAt(i),i);
-            curMax = Math.max(curMax,i-j+1);
+            map.put(s.charAt(i), i);
+            curMax = Math.max(curMax, i - j + 1);
         }
         return curMax;
     }
