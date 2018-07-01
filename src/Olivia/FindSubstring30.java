ebc8bf251280d0b53e4de325fb625c6982c59bcd
Mirage09 <borismirage00@gmail.com>
2018-07-01 11:27:57 -0400

    Add Explanation

--- src/Olivia/FindSubstring30.java
+++ src/Olivia/FindSubstring30.java
@@ -8,39 +8,43 @@ import java.util.Map;
 public class FindSubstring30 {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> result = new ArrayList<>();
-        if(s==null||words.length==0)
+        if (s == null || words.length == 0)
             return result;
         int len = words[0].length();
         //map for words
-        Map<String,Integer> wordMap = new HashMap<>();
-        for(int i=0; i<words.length;i++){
-            if(wordMap.containsKey(words[i])){
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+
+            // this for loop's content can be replaced by this:
+            // wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+            // get word value, or put default value
+
+            if (wordMap.containsKey(words[i])) {
                 int val = wordMap.get(words[i]);
-                wordMap.put(words[i],val+1);
-            }
-            else{
-                wordMap.put(words[i],1);
+                wordMap.put(words[i], val + 1);
+            } else {
+                wordMap.put(words[i], 1);
             }
         }
-        for(int j=0;j<s.length()-words.length*len+1;j++){
-            //Map<String,Integer> copy = wordMap;
-            Map<String, Integer> copy = new HashMap<String, Integer>(wordMap);
-            for(int k = 0; k <words.length;j++){
-                String cur = s.substring(j+k*len,j+k*len+len);
-                if(copy.containsKey(cur)){
+        for (int j = 0; j < s.length() - words.length * len + 1; j++) {
+            Map<String, Integer> copy = new HashMap<>(wordMap);
+            for (int k = 0; k < words.length; k++) {
+                String cur = s.substring(j + k * len, j + k * len + len);
+                if (copy.containsKey(cur)) {
                     int count = copy.get(cur);
-                    if (count==1) {copy.remove(cur);}
-                    else {copy.put(cur,count-1);}
-                    if(copy.isEmpty()){
+                    if (count == 1) {
+                        copy.remove(cur);
+                    } else {
+                        copy.put(cur, count - 1);
+                    }
+                    if (copy.isEmpty()) {
                         result.add(j);
                         break;
                     }
-                }
-                else{
+                } else {
                     break;
                 }
             }
-
         }
         return result;
 
