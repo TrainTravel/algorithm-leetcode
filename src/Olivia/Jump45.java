427e7d726ea11595fd233cd6aaa506447d74ebf8
OliviaZhangy <Olivia_zhangy@outlook.com>
2018-07-04 00:14:47 -0700

    45 55
    Almost the same problem.
    (✪ω✪)

---
+++ src/Olivia/Jump45.java
@@ -0,0 +1,17 @@
+package Olivia;
+
+public class Jump45 {
+    public int jump(int[] nums) {
+        int start = 0;
+        int end = 0;
+        int cur_max = 0;
+        for(int i=0;i<nums.length-1;i++){
+            cur_max = Math.max(cur_max,nums[i]+i);
+            if(i==end){
+                start++;
+                end = cur_max;
+            }
+        }
+        return start;
+    }
+}
---
+++ src/Olivia/Jump55.java
@@ -0,0 +1,20 @@
+package Olivia;
+
+public class Jump55 {
+    public boolean canJump(int[] nums) {
+        int start = 0;
+        int end = 0;
+        int cur_max = 0;
+        for(int i=0;i<nums.length-1;i++){
+            cur_max = Math.max(cur_max,nums[i]+i);
+            if(i==end){
+                start++;
+                end = cur_max;
+            }
+        }
+        if (end<nums.length-1)
+            return false;
+        else
+            return true;
+    }
+    }
