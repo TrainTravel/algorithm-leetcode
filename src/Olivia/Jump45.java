0dd17a9ca6cd272b7800aeafe69595663ee3a74e
Mirage09 <borismirage00@gmail.com>
2018-07-04 11:59:16 -0400

    么么哒～

--- src/Olivia/Jump45.java
+++ src/Olivia/Jump45.java
@@ -5,9 +5,9 @@ public class Jump45 {
         int start = 0;
         int end = 0;
         int cur_max = 0;
-        for(int i=0;i<nums.length-1;i++){
-            cur_max = Math.max(cur_max,nums[i]+i);
-            if(i==end){
+        for (int i = 0; i < nums.length - 1; i++) {
+            cur_max = Math.max(cur_max, nums[i] + i);
+            if (i == end) {
                 start++;
                 end = cur_max;
             }
--- src/Olivia/Jump55.java
+++ src/Olivia/Jump55.java
@@ -5,16 +5,16 @@ public class Jump55 {
         int start = 0;
         int end = 0;
         int cur_max = 0;
-        for(int i=0;i<nums.length-1;i++){
-            cur_max = Math.max(cur_max,nums[i]+i);
-            if(i==end){
+        for (int i = 0; i < nums.length - 1; i++) {
+            cur_max = Math.max(cur_max, nums[i] + i);
+            if (i == end) {
                 start++;
                 end = cur_max;
             }
         }
-        if (end<nums.length-1)
+        if (end < nums.length - 1)
             return false;
         else
             return true;
     }
-    }
+}
