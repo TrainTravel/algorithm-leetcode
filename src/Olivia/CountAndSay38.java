8bf3c18d49eb8b27bbe507db9b8bdb1e2a46290b
Mirage09 <borismirage00@gmail.com>
2018-07-05 10:33:09 -0400

    Minor edit

--- src/Olivia/CountAndSay38.java
+++ src/Olivia/CountAndSay38.java
@@ -3,21 +3,21 @@ package Olivia;
 public class CountAndSay38 {
     public String countAndSay(int n) {
         String s = "1";
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             s = countHelper(s);
         }
         return s;
 
     }
-    private String countHelper (String s){
+
+    private String countHelper(String s) {
         StringBuilder s1 = new StringBuilder();
         char cur = s.charAt(0);
         int count = 1;
-        for(int i=1;i<s.length();i++){
-            if (cur==s.charAt(i)){
+        for (int i = 1; i < s.length(); i++) {
+            if (cur == s.charAt(i)) {
                 count++;
-            }
-            else{
+            } else {
 
                 s1.append(count);
                 s1.append(cur);
--- src/Olivia/LongestValidParentheses32.java
+++ src/Olivia/LongestValidParentheses32.java
@@ -6,34 +6,31 @@ public class LongestValidParentheses32 {
     public int longestValidParentheses(String s) {
         int max_length = 0;
         Stack<Integer> s1 = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('){
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
                 s1.push(i);
-            }
-            else{
-                if(!s1.isEmpty()){
-                    if(s.charAt(s1.peek())=='('){
+            } else {
+                if (!s1.isEmpty()) {
+                    if (s.charAt(s1.peek()) == '(') {
                         s1.pop();
-                    }
-                    else{
+                    } else {
                         s1.push(i);
                     }
                 }
             }
         }
-        if(s1.isEmpty()){
+        if (s1.isEmpty()) {
             max_length = s.length();
-        }
-        else{
+        } else {
             int endLoc = s.length();
             int startLoc = 0;
-            while(!s1.isEmpty()){
+            while (!s1.isEmpty()) {
                 startLoc = s1.peek();
                 s1.pop();
-                max_length = Math.max(max_length,endLoc-startLoc-1);
+                max_length = Math.max(max_length, endLoc - startLoc - 1);
                 endLoc = startLoc;
             }
-            max_length = Math.max(max_length,endLoc);
+            max_length = Math.max(max_length, endLoc);
         }
         return max_length;
 
