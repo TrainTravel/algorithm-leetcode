fc6508b2995b570c9bb7af19c7fb404c6669a156
Mirage09 <borismirage00@gmail.com>
2018-07-04 21:17:17 -0400

    Add Explanation

--- src/Mirage/LeetCodeSolution/AddTwoNumbers.java
+++ src/Mirage/LeetCodeSolution/AddTwoNumbers.java
@@ -1,6 +1,18 @@
 package Mirage.LeetCodeSolution;
 
 /**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ *
  * @author BorisMirage
  * Time: 11/24/17 12:26
  * Created with IntelliJ IDEA
@@ -10,27 +22,24 @@ package Mirage.LeetCodeSolution;
 public class AddTwoNumbers {
 
     /**
-     * You are given two non-empty linked lists representing two non-negative integers.
-     * The digits are stored in reverse order and each of their nodes contain a single digit.
-     * Add the two numbers and return it as a linked list.
-     * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-     * <p>
-     * Definition for singly-linked list.
-     * public class ListNode {
-     * int val;
-     * ListNode next;
-     * ListNode(int x) { val = x; }
-     * }
+     * Sum two nodes and return the sum node. Until List is empty
      *
      * @param l1 first int contains in ListNode
      * @param l2 second int contains in ListNode
      * @return result in ListNode
      */
-    /* Sum two nodes and return the sum node. Until List is empty */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         return recursiveAdding(l1, l2, 0);
     }
 
+    /**
+     * Recursively add nodes until reach the end of list.
+     *
+     * @param l1  first node
+     * @param l2  second node
+     * @param sum previous sum
+     * @return
+     */
     public ListNode recursiveAdding(ListNode l1, ListNode l2, int sum) {
 
         /* Special case and end point */
