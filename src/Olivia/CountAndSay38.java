d544f2fa5328a4e1434dddb8642eefe4f926da6d
OliviaZhangy <Olivia_zhangy@outlook.com>
2018-07-05 03:21:24 -0700

    32 38
    32有bug
    不查了睡觉~~

--- src/Olivia/CountAndSay38.java
+++ src/Olivia/CountAndSay38.java
@@ -2,6 +2,32 @@ package Olivia;
 
 public class CountAndSay38 {
     public String countAndSay(int n) {
-        return "";
+        String s = "1";
+        for(int i = 0; i<n; i++){
+            s = countHelper(s);
+        }
+        return s;
+
+    }
+    private String countHelper (String s){
+        StringBuilder s1 = new StringBuilder();
+        char cur = s.charAt(0);
+        int count = 1;
+        for(int i=1;i<s.length();i++){
+            if (cur==s.charAt(i)){
+                count++;
+            }
+            else{
+
+                s1.append(count);
+                s1.append(cur);
+                cur = s.charAt(i);
+                count = 1;
+            }
+        }
+
+        s1.append(count);
+        s1.append(cur);
+        return s1.toString();
     }
 }
---
+++ src/Olivia/LongestValidParentheses32.java
@@ -0,0 +1,41 @@
+package Olivia;
+
+import java.util.Stack;
+
+public class LongestValidParentheses32 {
+    public int longestValidParentheses(String s) {
+        int max_length = 0;
+        Stack<Integer> s1 = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                s1.push(i);
+            }
+            else{
+                if(!s1.isEmpty()){
+                    if(s.charAt(s1.peek())=='('){
+                        s1.pop();
+                    }
+                    else{
+                        s1.push(i);
+                    }
+                }
+            }
+        }
+        if(s1.isEmpty()){
+            max_length = s.length();
+        }
+        else{
+            int endLoc = s.length();
+            int startLoc = 0;
+            while(!s1.isEmpty()){
+                startLoc = s1.peek();
+                s1.pop();
+                max_length = Math.max(max_length,endLoc-startLoc-1);
+                endLoc = startLoc;
+            }
+            max_length = Math.max(max_length,endLoc);
+        }
+        return max_length;
+
+    }
+}
