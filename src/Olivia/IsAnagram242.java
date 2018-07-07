d30c648213570d084e1516ab1eb90542790c2364
OliviaZhangy <Olivia_zhangy@outlook.com>
2018-07-07 01:04:07 -0700

    242

---
+++ src/Olivia/IsAnagram242.java
@@ -0,0 +1,21 @@
+package Olivia;
+
+public class IsAnagram242 {
+    public boolean isAnagram(String s, String t) {
+        int[] result = new int[26];
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(int i=0;i<s.length();i++){
+            result[s.charAt(i)-'a']++;
+        }
+        for(int i =0;i<t.length();i++){
+            result[t.charAt(i)-'a']--;
+            if (result[t.charAt(i)-'a']<0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
