45721dbfd0751765fec37d2406b72840d4af406d
Mirage09 <borismirage00@gmail.com>
2018-07-08 13:13:54 -0400

    Minor edit

--- src/Olivia/GroupAnagrams49.java
+++ src/Olivia/GroupAnagrams49.java
@@ -5,12 +5,12 @@ import java.util.*;
 public class GroupAnagrams49 {
     public List<List<String>> groupAnagrams(String[] strs) {
         ArrayList<List<String>> result = new ArrayList<>();
-        if(strs.length==0){
+        if (strs.length == 0) {
             return result;
         }
-        Map<String,List<String>> map = new HashMap<>();
-        for(String cur:strs){
-            char[]key = cur.toCharArray();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String cur : strs) {
+            char[] key = cur.toCharArray();
             Arrays.sort(key);
             String keyStr = String.valueOf(key);
             if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<String>());
