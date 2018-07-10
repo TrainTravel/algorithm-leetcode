1ba9570dd7926e594ad7617bbeaed8779ff5d9ae
Mirage09 <borismirage00@gmail.com>
2018-07-09 20:41:06 -0400

    Minor edit

--- src/Olivia/GenerateParenthesis22.java
+++ src/Olivia/GenerateParenthesis22.java
@@ -6,19 +6,20 @@ import java.util.List;
 public class GenerateParenthesis22 {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        generateHelper(result,"",0,0,n);
+        generateHelper(result, "", 0, 0, n);
         return result;
     }
-    private void generateHelper(List<String> result,String str, int left,int right, int max ){
-        if(str.length()==max*2){
+
+    private void generateHelper(List<String> result, String str, int left, int right, int max) {
+        if (str.length() == max * 2) {
             result.add(str);
             return;
         }
-        if(left<max){
-            generateHelper(result,str+"(",left+1,right,max);
+        if (left < max) {
+            generateHelper(result, str + "(", left + 1, right, max);
         }
-        if(right<left){
-            generateHelper(result,str+")",left,right+1,max);
+        if (right < left) {
+            generateHelper(result, str + ")", left, right + 1, max);
         }
     }
 }
