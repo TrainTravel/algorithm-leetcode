38b2f231b847a118ba00db79bc7ed350b5703584
Mirage09 <borismirage00@gmail.com>
2018-07-10 21:57:50 -0400

    Minor Edit

--- src/Olivia/CombinationSum240.java
+++ src/Olivia/CombinationSum240.java
@@ -8,23 +8,23 @@ public class CombinationSum240 {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<>();
-        combinationHelper(candidates,target,result,new ArrayList<Integer>(),0);
+        combinationHelper(candidates, target, result, new ArrayList<Integer>(), 0);
         return result;
     }
-    private void combinationHelper (int[] candidates, int target, List<List<Integer>> result, List<Integer>cur,int startLoc){
-        if(target>0){
-            for(int i = startLoc;i<candidates.length;i++){
-                while(i>startLoc&&i<candidates.length&&candidates[i]==candidates[i-1]){
+
+    private void combinationHelper(int[] candidates, int target, List<List<Integer>> result, List<Integer> cur, int startLoc) {
+        if (target > 0) {
+            for (int i = startLoc; i < candidates.length; i++) {
+                while (i > startLoc && i < candidates.length && candidates[i] == candidates[i - 1]) {
                     i++;
                 }
-                if(i==candidates.length)break;
+                if (i == candidates.length) break;
                 cur.add(candidates[i]);
-                combinationHelper(candidates,target-candidates[i],result,cur,i+1);
-                cur.remove(cur.size()-1);
+                combinationHelper(candidates, target - candidates[i], result, cur, i + 1);
+                cur.remove(cur.size() - 1);
             }
 
-        }
-        else if(target==0){
+        } else if (target == 0) {
             result.add(new ArrayList<>(cur));
         }
     }
--- src/Olivia/Insert57.java
+++ src/Olivia/Insert57.java
@@ -18,17 +18,17 @@ public class Insert57 {
             end = e;
         }
     }
+
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         intervals.add(newInterval);
         List<Interval> result = new ArrayList<>();
         intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
-        for(int i=0;i<intervals.size();i++){
-            if(i>0&&intervals.get(i).start>=result.get(result.size()-1).start&&intervals.get(i).start<=result.get(result.size()-1).end){
-                Interval cur  = new Interval(Math.min(result.get(result.size()-1).start,intervals.get(i).start),Math.max(result.get(result.size()-1).end,intervals.get(i).end));
-                result.remove(result.size()-1);
+        for (int i = 0; i < intervals.size(); i++) {
+            if (i > 0 && intervals.get(i).start >= result.get(result.size() - 1).start && intervals.get(i).start <= result.get(result.size() - 1).end) {
+                Interval cur = new Interval(Math.min(result.get(result.size() - 1).start, intervals.get(i).start), Math.max(result.get(result.size() - 1).end, intervals.get(i).end));
+                result.remove(result.size() - 1);
                 result.add(cur);
-            }
-            else{
+            } else {
                 result.add(intervals.get(i));
             }
         }
