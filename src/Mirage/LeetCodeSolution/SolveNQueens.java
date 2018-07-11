138ee0a9c700f5bb8df687e1345a585b6d5b4b09
Mirage09 <borismirage00@gmail.com>
2018-07-10 23:00:56 -0400

    N-Queens init commit

---
+++ src/Mirage/LeetCodeSolution/SolveNQueens.java
@@ -0,0 +1,44 @@
+package Mirage.LeetCodeSolution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+ * Given an integer n, return all distinct solutions to the n-queens puzzle.
+ * Each solution contains a distinct board configuration of the n-queens' placement.
+ * 'Q' and '.' both indicate a queen and an empty space respectively.
+ * <p>
+ * Example:
+ * Input: 4
+ * Output: [
+ * [".Q..",  // Solution 1
+ * "...Q",
+ * "Q...",
+ * "..Q."],
+ * <p>
+ * ["..Q.",  // Solution 2
+ * "Q...",
+ * "...Q",
+ * ".Q.."]
+ * ]
+ * Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
+ *
+ * @author BorisMirage
+ * Time: 2018/07/10 22:26
+ * Created with IntelliJ IDEA
+ */
+
+public class SolveNQueens {
+    /**
+     * Use backtracking to solve n queens puzzle.
+     * Exit recursion if there exist more than n queens.
+     *
+     * @param n placing n queens
+     * @return List contains all solutions that distinct board configuration of the n-queens' placement
+     */
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        return res;
+    }
+}
