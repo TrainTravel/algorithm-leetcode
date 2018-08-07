b3cea2946f51e565d3fe0401facc2f26a223c38f
Mirage09 <borismirage00@gmail.com>
2018-08-07 16:57:14 -0700

    Set Matrix Zeroes

--- src/Mirage/LeetCodeSolution/SetZeroes.java
+++ src/Mirage/LeetCodeSolution/SetZeroes.java
@@ -13,32 +13,57 @@ import java.util.Arrays;
 
 public class SetZeroes {
     /**
-     * Mark as -1.
+     * Leave first row and column as the sign to mark which row and column needs to be set 0.
+     * Two boolean value to record if first row and column needs to be set as 0.
      *
      * @param matrix given matrix
      */
     public void setZeroes(int[][] matrix) {
+
+        /* Use 2 boolean to mark whether first row and column needs to be set 0 */
+        boolean row0 = false;
+        boolean col0 = false;
+
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
-                    /* Mark column */
-                    for (int k = 0; k < matrix[0].length; k++) {
-                        matrix[i][k] = -1;
+                    if (i == 0) {
+                        col0 = true;
                     }
-                    for (int k = 0; k < matrix.length; k++) {
-                        matrix[k][j] = -1;
+                    if (j == 0) {
+                        row0 = true;
                     }
+
+                    /* Mark which row and column needs to be set 0 */
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+
                 }
             }
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == -1) {
+        /* Set 0 for each needed row and column except first row and column */
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
+
+        /* First column and row */
+        if (row0) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (col0) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
         System.out.println(Arrays.deepToString(matrix));
     }
 }
