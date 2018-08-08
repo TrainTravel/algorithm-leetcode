97e702cce0d4aeb81235bb090feb28648666d91e
Mirage09 <borismirage00@gmail.com>
2018-08-07 23:51:26 -0700

    Update file with minor edit

--- src/Olivia/ClimbStairs70.java
+++ src/Olivia/ClimbStairs70.java
@@ -2,6 +2,7 @@ package Olivia;
 
 public class ClimbStairs70 {
     public int climbStairs(int n) {
+
         /* special case for n=0,1,2 */
         if (n == 0)
             return 0;
@@ -9,9 +10,11 @@ public class ClimbStairs70 {
             return 1;
         if (n == 2)
             return 2;
+
         int[] dp = new int[n];
         dp[0] = 1;
         dp[1] = 2;
+
         /* each element represent the number of ways */
         for (int i = 2; i < n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
