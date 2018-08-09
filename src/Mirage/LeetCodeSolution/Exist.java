938fe4b39e6a1b3399e6ff92901772f9d5c8937e
Mirage09 <borismirage00@gmail.com>
2018-08-09 16:25:56 -0700

    Update file with minor edit

--- src/Mirage/LeetCodeSolution/Exist.java
+++ src/Mirage/LeetCodeSolution/Exist.java
@@ -31,11 +31,8 @@ public class Exist {
      */
     public boolean exist(char[][] board, String word) {
 
-        int m = board.length;
-        int n = board[0].length;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == word.charAt(0)) {
                     if (tracking(board, word, i, j, 0)) {
                         return true;
