9bbc003292e24780710b1648e68e971136d9f54a
Null <borismirage00@gmail.com>
2018-10-02 12:34:56 -0400

    Same Tree

---
+++ src/Mirage/LeetCodeSolution/IsSameTree.java
@@ -0,0 +1,53 @@
+package Mirage.LeetCodeSolution;
+
+/**
+ * Given two binary trees, write a function to check if they are the same or not.
+ * Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+ *
+ * @author BorisMirage
+ * Time: 2018/10/02 12:10
+ * Created with IntelliJ IDEA
+ */
+
+public class IsSameTree {
+    /**
+     * Recursion.
+     * If sub nodes do not be in same situation (null or not null), return false.
+     * If value in tree node is same, continue recursion in sub nodes.
+     *
+     * @param p TreeNode 1
+     * @param q TreeNode 2
+     * @return if two trees are same
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+
+    /**
+     * TreeNode structure
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        /**
+         * Value in TreeNode
+         *
+         * @param x value
+         */
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
+
