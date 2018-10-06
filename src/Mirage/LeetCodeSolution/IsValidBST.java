a1d6cbd732013a77ef154b9ea6bcedfd7aaee9e2
Null <borismirage00@gmail.com>
2018-10-06 16:45:06 -0400

    Validate Binary Search Tree

--- src/Mirage/LeetCodeSolution/IsValidBST.java
+++ src/Mirage/LeetCodeSolution/IsValidBST.java
@@ -1,7 +1,5 @@
 package Mirage.LeetCodeSolution;
 
-import java.util.LinkedList;
-import java.util.Stack;
 
 /**
  * Given a binary tree, determine if it is a valid binary search tree (BST).
@@ -16,27 +14,32 @@ import java.util.Stack;
  */
 
 public class IsValidBST {
+
+    /**
+     * Recursion with each root's value. Compare left subtree and right subtree.
+     *
+     * @param root root TreeNode
+     * @return is this tree a BST
+     */
     public boolean isValidBST(TreeNode root) {
-        LinkedList<Integer> res = new LinkedList<>();
-        Stack<TreeNode> temp = new Stack<>();
-        TreeNode cur = root;
+        return recursion(root, Long.MIN_VALUE, Long.MAX_VALUE);     // avoid int overflow
+    }
 
-        /* Traverse process */
-        while (cur != null || !temp.isEmpty()) {
-            while (cur != null) {
-                temp.add(cur);
-                cur = cur.left;
-            }
-            cur = temp.pop();
-            res.add(cur.val);
-            cur = cur.right;
+    /**
+     * Recursion that compare root's value to left and right.
+     *
+     * @param root current root
+     * @param min  min value
+     * @param max  max value
+     * @return is current root a BST
+     */
+    private boolean recursion(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
         }
-
-        for (int i = 0; i < res.size() - 1; i++) {
-            if (res.get(i) >= res.get(i + 1)) {
-                return false;
-            }
+        if (root.val >= max || root.val <= min) {
+            return false;
         }
-        return true;
+        return recursion(root.left, min, root.val) && recursion(root.right, root.val, max);
     }
 }
