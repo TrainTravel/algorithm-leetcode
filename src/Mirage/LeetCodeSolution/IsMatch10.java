faeaf06da026249b1e6385039bdc841cd0abde96
Null <borismirage00@gmail.com>
2018-10-11 21:59:58 -0400

    Regular Expression Matching

---
+++ src/Mirage/LeetCodeSolution/IsMatch10.java
@@ -0,0 +1,50 @@
+package Mirage.LeetCodeSolution;
+
+/**
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ * Note:
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ *
+ * @author BorisMirage
+ * Time: 2018/10/11 21:25
+ * Created with IntelliJ IDEA
+ */
+
+public class IsMatch10 {
+    /**
+     * @param s string
+     * @param p pattern string
+     * @return if string is matched to pattern
+     */
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i - 1]) {
+                dp[0][i + 1] = true;
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.'||p.charAt(j) == s.charAt(i)) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1];
+                    }
+                }
+            }
+        }
+
+        return dp[s.length()][p.length()];
+    }
+}
