ed4e004051ebe0314c45a8a0aeb4174edde5467d
Null <borismirage00@gmail.com>
2018-10-13 10:46:10 -0400

    Add Explanation

--- src/Mirage/LeetCodeSolution/TwoSumStructure.java
+++ src/Mirage/LeetCodeSolution/TwoSumStructure.java
@@ -3,6 +3,10 @@ package Mirage.LeetCodeSolution;
 import java.util.*;
 
 /**
+ * Design and implement a TwoSum class. It should support the following operations: add and find.
+ * add - Add the number to an internal data structure.
+ * find - Find if there exists any pair of numbers which sum is equal to the value.
+ *
  * @author BorisMirage
  * Time: 2018/10/11 22:04
  * Created with IntelliJ IDEA
@@ -12,21 +16,27 @@ public class TwoSumStructure {
     HashMap<Integer, Integer> temp;
 
     /**
-     * Initialize your data structure here.
+     * Initialize data structure.
      */
     public TwoSumStructure() {
         temp = new HashMap<>();
     }
 
     /**
-     * Add the number to an internal data structure..
+     * Add value to structure.
+     *
+     * @param number add value
      */
     public void add(int number) {
         temp.put(number, temp.getOrDefault(number, 0) + 1);
     }
 
+
     /**
      * Find if there exists any pair of numbers which sum is equal to the value.
+     *
+     * @param value given value
+     * @return if two sum value can be found in this structure
      */
     public boolean find(int value) {
         for (Map.Entry<Integer, Integer> entry : temp.entrySet()) {
