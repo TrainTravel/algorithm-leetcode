10479542352fef7b9eaf12baa47bfbbece91d4ea
Null <borismirage00@gmail.com>
2019-02-11 22:26:55 -0500

    Reduce runtime

--- src/Mirage/LeetCodeSolution/MaxProfit_188.java
+++ src/Mirage/LeetCodeSolution/MaxProfit_188.java
@@ -12,11 +12,11 @@ package Mirage.LeetCodeSolution;
 public class MaxProfit_188 {
     /**
      * Dynamic programming with table.
-     * [i, j] = max([i, j - 1], prices[j] - prices[jj] + [i - 1, jj])
+     * [i, j] = max([i, j - 1], prices[j] - prices[j'] + [i - 1, j'])
      * i: ith transaction
      * j: prices[j], index in int array price
-     * jj: in range of [0, j-1]
-     * => [i, j] = max(dp[i, j - 1], prices[j] + max(dp[i - 1, jj] - prices[jj]))
+     * j': in range of [0, j-1]
+     * => [i, j] = max([i, j - 1], prices[j] + max([i - 1, j'] - prices[j']))  (efficient for looping)
      * dp[0, j] = 0
      * dp[i, 0] = 0
      *
@@ -26,29 +26,39 @@ public class MaxProfit_188 {
      */
     public int maxProfit(int k, int[] prices) {
 
-        if (prices.length < 1) {
+        if (prices.length < 2) {
             return 0;
         }
 
-        if (k >= prices.length / 2) {
-            int temp = 0;
+        /* Special case: compare between current and previous value in array */
+        if (k >= prices.length / 2) {       // k is sufficient for comparing each adjacent value in array
+            int max = 0;
             for (int i = 1; i < prices.length; i++) {
-                if (prices[i] > prices[i - 1])
-                    temp += prices[i] - prices[i - 1];
+                if (prices[i] - prices[i - 1] > 0) {
+                    max += prices[i] - prices[i - 1];
+                }
             }
-            return temp;
+            return max;
         }
 
-        int[][] t = new int[k + 1][prices.length];
+        int[] r1 = new int[prices.length];
+        int[] r2 = new int[prices.length];
 
-        for (int i = 1; i < k + 1; i++) {
-            int temp = t[i - 1][0] - prices[0];
-            for (int j = 1; j < prices.length; j++) {
-                t[i][j] = Math.max(t[i][j - 1], prices[j] + temp);
-                temp = Math.max(temp, t[i - 1][j] - prices[j]);
+        for (int i = 0; i < k + 1; i++) {
+            int base = -prices[0];
+            for (int j = 0; j < r1.length; j++) {
+                r2[j] = Integer.max(r2[j - 1], prices[j] + base);
+                base = Integer.max(base, r1[j] - prices[j]);
+
+            }
+
+            /* Reduce memory usage */
+            for (int j = 0; j < prices.length; j++) {
+                r1[j] = r2[j];
+                r2[j] = 0;
             }
         }
 
-        return t[k][prices.length - 1];
+        return r2[prices.length - 1];
     }
 }
