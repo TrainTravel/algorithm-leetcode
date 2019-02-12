68737b02442e87252b63e484dd5ebbe242fd4e69
Null <borismirage00@gmail.com>
2019-02-12 17:43:24 -0500

    Single Number

---
+++ src/Mirage/LeetCodeSolution/SingleNumber_136.java
@@ -0,0 +1,25 @@
+package Mirage.LeetCodeSolution;
+
+/**
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * @author BorisMirage
+ * Time: 2019/02/12 17:30
+ * Created with IntelliJ IDEA
+ */
+
+public class SingleNumber_136 {
+    /**
+     * XOR each adjacent number and the last number is the result
+     *
+     * @param nums int array
+     * @return number appears only once
+     */
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;     // bitwise XOR
+        }
+        return res;
+    }
+}
