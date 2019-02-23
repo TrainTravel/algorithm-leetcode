20a0d0afd8179c4bfa60adcc32e25e3017b8f469
Null <borismirage00@gmail.com>
2019-02-23 12:25:59 -0500

    Add Explanation

--- src/Solution/Array/FindMedianSortedArrays_4.java
+++ src/Solution/Array/FindMedianSortedArrays_4.java
@@ -1,10 +1,9 @@
 package Solution.Array;
 
 /**
- * There are two sorted arrays nums1 and nums2 of size m and n respectively.
- * Find the median of the two sorted arrays.
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively, find the median of the two sorted arrays.
  * The overall run time complexity should be O(log (m+n)).
- * It can be assumed that nums1 and nums2 cannot be both empty.
+ * nums1 and nums2 cannot be both empty.
  *
  * @author BorisMirage
  * Time: 2018/03/25 12:17
@@ -14,6 +13,10 @@ package Solution.Array;
 public class FindMedianSortedArrays_4 {
     /**
      * Two arrays are both SORTED.
+     * Add a virtual space in array, for example:
+     * [#, 1, #, 3, #, 6, #, 10, #, 11, #, 21, #]
+     * In this way the median of this array will be (nums[(l + r) / 2] + nums[(l + r - 1) / 2]) / 2.
+     * l = 0, r = nums.length.
      *
      * @param nums1 first sorted array with length m
      * @param nums2 second sorted array with length n
@@ -24,25 +27,32 @@ public class FindMedianSortedArrays_4 {
         int length2 = nums2.length;
 
         if (length1 < length2) {
-            return findMedianSortedArrays(nums2, nums1);        // assure num1 is larger array
+            return findMedianSortedArrays(nums2, nums1);        // assure num1 is longer array
         }
 
-        int low = 0, high = length2 * 2;        // nums 2 is shorter array, ensure not move out of bound
+        int low = 0, high = length2 * 2;        // avoid moving out of bound due to nums 2 is shorter array
+
         while (low <= high) {
             int mid2 = (high + low) / 2;
             int mid1 = length1 + length2 - mid2;
 
-            double l1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[(mid1 - 1) / 2];
-            double l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[(mid2 - 1) / 2];
-            double r1 = (mid1 == length1 * 2) ? Integer.MAX_VALUE : nums1[(mid1) / 2];
-            double r2 = (mid2 == length2 * 2) ? Integer.MAX_VALUE : nums2[(mid2) / 2];
+            /* Avoid no overlapping */
+            double left1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[(mid1 - 1) / 2];
+            double right1 = (mid1 == length1 * 2) ? Integer.MAX_VALUE : nums1[(mid1) / 2];
 
-            if (l1 > r2) {
+            /* Avoid second array is empty */
+            double left2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[(mid2 - 1) / 2];
+            double right2 = (mid2 == length2 * 2) ? Integer.MAX_VALUE : nums2[(mid2) / 2];
+
+            /* Ensure L1 <= R2 && L2 <= R1 */
+            if (left1 > right2) {
                 low = mid2 + 1;
-            } else if (r1 < l2) {
+            } else if (right1 < left2) {
                 high = mid2 - 1;
             } else {
-                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2;
+
+                /* median: (max(L1, L2) + min(R1, R2)) / 2 */
+                return (Math.max(left1, left2) + Math.min(right1, right2)) / 2;
             }
         }
         return -1;
