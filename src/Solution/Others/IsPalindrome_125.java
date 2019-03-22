39c55a11cd62e0559607a7fac8c629c348b7c1c9
Null <borismirage00@gmail.com>
2019-03-22 19:19:00 -0400

    Valid Palindrome

---
+++ src/Solution/Others/IsPalindrome_125.java
@@ -0,0 +1,47 @@
+package Solution.Others;
+
+/**
+ * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ * Empty string considered as palindrome string.
+ *
+ * @author BorisMirage
+ * Time: 2019/03/22 18:40
+ * Created with IntelliJ IDEA
+ */
+
+public class IsPalindrome_125 {
+    /**
+     * Traverse string with 2 indexes from start and end.
+     *
+     * @param s given string
+     * @return is s a palindrome string
+     */
+    public boolean isPalindrome(String s) {
+
+        /* Special case*/
+        if (s.length() == 0) {
+            return true;
+        }
+
+        int start = 0, end = s.length() - 1;
+
+        while (start < end) {
+
+            if (Character.isLetterOrDigit(s.charAt(start)) && Character.isLetterOrDigit(s.charAt(end))) {
+                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                    return false;
+                }
+                start += 1;
+                end -= 1;
+            } else {
+                if (!Character.isLetterOrDigit(s.charAt(start))) {
+                    start += 1;
+                }
+                if (!Character.isLetterOrDigit(s.charAt(end))) {
+                    end -= 1;
+                }
+            }
+        }
+        return true;
+    }
+}
