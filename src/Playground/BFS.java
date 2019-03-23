1fc568cd9d7883eda74399e747b3764c94505f2f
Null <borismirage00@gmail.com>
2019-03-23 18:35:35 -0400

    Breadth-First Search

---
+++ src/Playground/BFS.java
@@ -0,0 +1,54 @@
+package Playground;
+
+import Lib.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/03/23 18:20
+ * Created with IntelliJ IDEA
+ */
+
+public class BFS {
+    /**
+     * Breadth-first search implementation via tree node.
+     *
+     * @param root root node
+     * @return all elements in binary tree in BFS order (left first)
+     */
+    public List<List<Integer>> breadthFirstSearch(TreeNode root) {
+
+        List<List<Integer>> out = new LinkedList<>();
+
+        if (root == null) {
+            return out;
+        }
+
+        LinkedList<TreeNode> layer = new LinkedList<>();
+        layer.add(root);
+        while (!layer.isEmpty()) {
+
+            int size = layer.size();
+            List<Integer> temp = new LinkedList<>();
+
+            /* Tree level iteration */
+            for (int i = 0; i < size; i++) {
+                TreeNode c = layer.getFirst();
+                if (c.left != null) {
+                    layer.addLast(c.left);
+                }
+                if (c.right != null) {
+                    layer.addLast(c.right);
+                }
+                temp.add(c.val);
+                layer.removeFirst();
+            }
+
+            /* Add layer */
+            out.add(temp);
+        }
+        return out;
+    }
+}
