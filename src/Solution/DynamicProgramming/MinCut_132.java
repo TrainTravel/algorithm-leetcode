7566112b1f2de90f8cf71e0bb7528cb68a101385
Null <borismirage00@gmail.com>
2019-03-24 11:32:30 -0400

    Change example

--- src/Solution/DynamicProgramming/MinCut_132.java
+++ src/Solution/DynamicProgramming/MinCut_132.java
@@ -5,9 +5,9 @@ package Solution.DynamicProgramming;
  * Given a string s, partition s such that every substring of the partition is a palindrome.
  * Return the minimum cuts needed for a palindrome partitioning of s.
  * Example:
- * Input: "aab"
- * Output: 1
- * Explanation: The palindrome partitioning ["aa","b"] could be produced using 1 cut.
+ * Input: "cabababcbc"
+ * Output: 3
+ * Explanation: The palindrome partitioning ["c", "a", "babab", "cbc"] could be produced using 3 cut.
  *
  * @author BorisMirage
  * Time: 2019/03/24 10:48
