b74439b759de302aba6f901b8a7dbeae3b0c82e8
Null <borismirage00@gmail.com>
2019-04-30 10:35:12 -0400

    Add explanation

--- src/Solution/Tree/Connect_117.java
+++ src/Solution/Tree/Connect_117.java
@@ -5,17 +5,29 @@ import Lib.Node;
 import java.util.LinkedList;
 
 /**
+ * Given a binary tree (not perfect).
+ * Populate each next pointer to point to its next right node.
+ * If there is no next right node, the next pointer should be set to NULL.
+ * Initially, all next pointers are set to NULL.
+ *
  * @author BorisMirage
  * Time: 2019/04/30 09:53
  * Created with IntelliJ IDEA
  */
 
 public class Connect_117 {
+    /**
+     * Use queue to store each node in current and next layer.
+     *
+     * @param root root node
+     * @return populate tree
+     */
     public Node connect(Node root) {
 
         if (root == null) {
             return null;
         }
+
         LinkedList<Node> layer = new LinkedList<>();
         layer.add(root);
 
@@ -29,6 +41,8 @@ public class Connect_117 {
                 if (next.right != null) {
                     layer.add(next.right);
                 }
+
+                /* last node is initially set to null */
                 if (i < s - 1) {
                     next.next = layer.peek();
                 }
