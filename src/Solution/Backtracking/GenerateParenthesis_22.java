6c55adb4a7c10d1dc3e91b5904bdcc0882d8e1d3
Null <borismirage00@gmail.com>
2019-06-01 12:22:13 -0700

    Redo

--- src/Solution/Backtracking/GenerateParenthesis_22.java
+++ src/Solution/Backtracking/GenerateParenthesis_22.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
  * @author BorisMirage
  * Time: 2018/06/13 10:21
  * Created with IntelliJ IDEA
@@ -11,31 +13,46 @@ import java.util.List;
 
 public class GenerateParenthesis_22 {
     /**
-     * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-     * <p>
-     * Use recursion to insert pair of parentheses in one pair of parentheses's left and right, respectively.
+     * Backtracking.
      *
      * @param n pairs of parentheses
      * @return linked list that contains all combinations of parentheses
      */
 
     public List<String> generateParenthesis(int n) {
-        LinkedList<String> ans = new LinkedList<>();
+        List<String> output = new LinkedList<>();
+
+        /* Corner case */
         if (n == 0) {
-
-            /* Pair 0 - add nothing */
-            ans.add("");
-        } else {
-            for (int c = 0; c < n; c++)
-
-                /* Recursion iterator */
-                for (String left : generateParenthesis(c)) {
-                    for (String right : generateParenthesis(n - 1 - c)) {
-                        ans.add("(" + left + ")" + right);
-                    }
-                }
+            return output;
         }
-        return ans;
+
+        backtracking(output, "", 0, 0, 2 * n, n);
+        return output;
+    }
+
+    /**
+     * Backtracking to generate all parentheses.
+     * Construct pairs of parentheses by adding left part first, then insert right part by backtracking.
+     * Finally, if string length reaches the pair limit, add it to final result.
+     *
+     * @param output output list
+     * @param s      current string
+     * @param left   left length
+     * @param right  right length
+     * @param length max length of n pairs of parentheses (2 * n)
+     * @param pairs  n pairs
+     */
+    private void backtracking(List<String> output, String s, int left, int right, int length, int pairs) {
+        if (s.length() == length) {
+            output.add(s);      // endpoint
+        }
+        if (left < pairs) {     // each parenthesis pair has a left and a right
+            backtracking(output, s + "(", left + 1, right, length, pairs);
+        }
+        if (right < left) {     // if there is a left, then there must contains a right
+            backtracking(output, s + ")", left, right + 1, length, pairs);
+        }
     }
 
     public static void main(String[] args) {
