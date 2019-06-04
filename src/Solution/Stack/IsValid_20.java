eaed087eca3eef37c29df80526cd90e4edac7742
Null <borismirage00@gmail.com>
2019-06-04 11:09:41 -0700

    Use switch statement instead of if statement

--- src/Solution/Stack/IsValid_20.java
+++ src/Solution/Stack/IsValid_20.java
@@ -26,16 +26,24 @@ public class IsValid_20 {
         Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
-            if (c == '(')
-                stack.push(')');
-            else if (c == '{')
-                stack.push('}');
-            else if (c == '[')
-                stack.push(']');
-            else if (stack.isEmpty() || stack.pop() != c)       // open brackets must be closed by same type of it
-                return false;
+            switch (c) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                default:
+                    if (stack.isEmpty() || stack.pop() != c) {      // open brackets must be closed by same type of it
+                        return false;
+                    }
+                    break;
+            }
         }
-        return stack.isEmpty();
+        return stack.isEmpty();     // including corner case
     }
 
     public static void main(String[] args) {
