524bea1e431ad690cf82714ca6918cfd82637009
Null <borismirage00@gmail.com>
2019-06-05 21:48:51 -0700

    Add explanation

--- src/Lib/Graph/Graph.java
+++ src/Lib/Graph/Graph.java
@@ -3,6 +3,8 @@ package Lib.Graph;
 import java.util.List;
 
 /**
+ * Definition of graph.
+ *
  * @author BorisMirage
  * Time: 2019/06/05 21:40
  * Created with IntelliJ IDEA
@@ -12,6 +14,10 @@ public class Graph {
     public int val;
     public List<Graph> neighbor;
 
+    /**
+     * @param _val      value store in cache
+     * @param _neighbor all linked node to current graph node
+     */
     public Graph(int _val, List<Graph> _neighbor) {
         val = _val;
         neighbor = _neighbor;
