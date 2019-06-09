bd4c17bf1965d4006100f481fac63efda075be04
Null <borismirage00@gmail.com>
2019-06-08 23:10:13 -0700

    Redo Maximum Subarray

--- src/Solution/Array/MaxSubArray_53.java
+++ src/Solution/Array/MaxSubArray_53.java
@@ -2,6 +2,9 @@ package Solution.Array;
 
 
 /**
+ * Given an integer array nums
+ * Find the contiguous sub array (containing at least one number) which has the largest sum and return its sum.
+ *
  * @author BorisMirage
  * Time: 2018/06/29 19:34
  * Created with IntelliJ IDEA
@@ -9,32 +12,34 @@ package Solution.Array;
 
 public class MaxSubArray_53 {
     /**
-     * Given an integer array nums
-     * Find the contiguous sub array (containing at least one number) which has the largest sum and return its sum.
+     * Basic dynamic programming.
+     * dp[i] = dp[i - 1] + nums[i], if dp[i - 1] + dp[i] > 0
+     * dp[i] = nums[i], otherwise
+     * max = Math.max(dp[i], max)
      *
      * @param nums input int array
      * @return max contiguous sub array sum
      */
     public int maxSubArray(int[] nums) {
 
-        /* Corner case */
+        /* Corner case*/
         if (nums.length == 0) {
             return 0;
         }
 
-        int maxSum = nums[0];
-        int currentSum = 0;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = Integer.MIN_VALUE;
 
-        /* Iterate each element in array */
-        for (int num : nums) {
-            currentSum += num;
-            maxSum = Integer.max(maxSum, currentSum);
-
-            /* If current sum <= 0, then contiguous sub array sum ends here and max sum will exclude current element */
-            if (currentSum < 0) {
-                currentSum = 0;
+        for (int i = 1; i < dp.length; i++) {
+            if (dp[i - 1] + dp[i] > 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
             }
+            max = Math.max(dp[i], max);
+
         }
-        return maxSum;
+        return max;
     }
 }
