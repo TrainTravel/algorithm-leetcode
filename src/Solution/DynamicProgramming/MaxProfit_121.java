a6153cf30164988b551d7af326854fa9c7b5bf8d
Null <borismirage00@gmail.com>
2019-06-09 10:48:17 -0700

    Redo Best Time to Buy and Sell Stock

--- src/Solution/DynamicProgramming/MaxProfit_121.java
+++ src/Solution/DynamicProgramming/MaxProfit_121.java
@@ -4,15 +4,6 @@ package Solution.DynamicProgramming;
  * Say you have an array for which the ith element is the price of a given stock on day i.
  * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
  * Note that you cannot sell a stock before you buy one.
- * Example 1:
- * Input: [7,1,5,3,6,4]
- * Output: 5
- * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
- * Not 7 - 1 = 6, as selling price needs to be larger than buying price.
- * Example 2:
- * Input: [7,6,4,3,1]
- * Output: 0
- * Explanation: In this case, no transaction is done, i.e. max profit = 0.
  *
  * @author BorisMirage
  * Time: 2019/01/22 11:02
@@ -21,19 +12,18 @@ package Solution.DynamicProgramming;
 
 public class MaxProfit_121 {
     /**
-     * Dynamic programming. Similiar to max subarray problem.
+     * Dynamic programming.
+     * Similar to max subarray problem.
      *
      * @param prices int array
      * @return max "profit"
      */
     public int maxProfit(int[] prices) {
 
-        int current = 0, max = 0;
+        int max = 0, profit = 0;
         for (int i = 1; i < prices.length; i++) {
-
-            /* Avoid negative */
-            current = Math.max(0, current + prices[i] - prices[i - 1]);
-            max = Math.max(current, max);
+            profit = Math.max(0, prices[i] - prices[i - 1] + profit);
+            max = Math.max(max, profit);
         }
         return max;
     }
