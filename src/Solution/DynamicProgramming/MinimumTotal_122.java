98336874c00fa0ab76c9d39efa08c9159de7a249
Null <borismirage00@gmail.com>
2019-06-09 10:25:23 -0700

    Redo Triangle

--- src/Solution/DynamicProgramming/MinimumTotal_122.java
+++ src/Solution/DynamicProgramming/MinimumTotal_122.java
@@ -4,14 +4,6 @@ import java.util.List;
 
 /**
  * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to ADJACENT numbers on the row below.
- * For example, given the following triangle
- * [
- * [2],
- * [3,4],
- * [6,5,7],
- * [4,1,8,3]
- * ]
- * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
  *
  * @author BorisMirage
  * Time: 2019/01/15 10:32
@@ -21,20 +13,33 @@ import java.util.List;
 public class MinimumTotal_122 {
 
     /**
-     * Bottom to top.
+     * Dynamic programming that find path from bottom to top.
+     * Note that only adjacent numbers on the row below is reachable.
+     * Therefore, simply compare two adjacent element will suffice.
      *
      * @param triangle given triangle
      * @return min distance
      */
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[] temp = new int[triangle.size() + 1];
 
-        for (int i = triangle.size() - 1; i >= 0; i--) {
+        /* Corner case */
+        if (triangle.size() == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[triangle.get(triangle.size() - 1).size()];
+
+        for (int i = 0; i < triangle.get(triangle.size() - 1).size(); i++) {
+            dp[i] = triangle.get(triangle.size() - 1).get(i);
+        }
+
+        for (int i = triangle.size() - 2; i > -1; i--) {
             for (int j = 0; j < triangle.get(i).size(); j++) {
-                temp[j] = Math.min(temp[j], temp[j + 1]) + triangle.get(i).get(j);
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
             }
         }
-        return temp[0];
+
+        return dp[0];
     }
 
 }
