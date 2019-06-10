9cc0fd797b431bf578b1c4b73a305343342b2cae
Null <borismirage00@gmail.com>
2019-06-10 13:34:56 -0700

    Minor modify

--- src/Solution/DynamicProgramming/NumTrees_96.java
+++ src/Solution/DynamicProgramming/NumTrees_96.java
@@ -14,10 +14,10 @@ public class NumTrees_96 {
      * Dynamic programming.
      * G(n) = F(1, n) + F(2, n) + ... + F(n, n).
      * G(n) is the actual function we need to calculate in order to solve the problem
-     * G(0)=1, G(1)=1.
+     * G(0) = 1, G(1) = 1.
      * F(i, n), 1 <= i <= n: the number of unique BST, where i is the root of BST, and the sequence ranges from 1 to n.
-     * F(i, n) = G(i-1) * G(n-i)
-     * ==> G(n) = G(0) * G(n-1) + G(1) * G(n-2) + … + G(n-1) * G(0)
+     * F(i, n) = G(i - 1) * G(n - i)
+     * ==> G(n) = G(0) * G(n - 1) + G(1) * G(n - 2) + … + G(n - 1) * G(0)
      *
      * @param n given n
      * @return number of unique BST
