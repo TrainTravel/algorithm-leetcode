1deb54acc7e0d9373e5b46ee6198b626caabd9b8
Null <borismirage00@gmail.com>
2019-06-09 22:30:23 -0700

    Redo Palindrome Partitioning II

--- src/Solution/DynamicProgramming/MinCut_132.java
+++ src/Solution/DynamicProgramming/MinCut_132.java
@@ -1,6 +1,5 @@
 package Solution.DynamicProgramming;
 
-
 /**
  * Given a string s, partition s such that every substring of the partition is a palindrome.
  * Return the minimum cuts needed for a palindrome partitioning of s.
@@ -18,43 +17,38 @@ public class MinCut_132 {
     /**
      * Dynamic programming with two 1D boolean array.
      * Assume i, j are the index in string, cut[i] is the min cut time for s(0, i).
-     * 1. Assume s from 0 to i is not palindrome, set cut[i] = i initially.
-     * 2. If s.char(j) == s.char(i) (j<=i) && s(j+1, i-1) is palindrome, then s(j, i) is palindrome.
-     * 3. If s(j, i) is palindrome and s(0, j-1) is palindrome, then cut[i] = min(cut[i], cut[j - 1] + 1)
+     * 1. cut[i] is the minimum of cut[j - 1] + 1 (j <= i), if [j, i] is palindrome.
+     * 2. If [j, i] is palindrome, then [j + 1, i - 1] is palindrome, and c[j] == c[i].
      *
      * @param s given string
      * @return min cuts for a palindrome partitioning of s
      */
     public int minCut(String s) {
 
+        /* Corner case */
         if (s.length() == 0) {
-            return -1;
+            return 0;
         }
-
-        int[] minCut = new int[s.length()];
-
-        /* dp1: is palindrome string from index 0 to i - 1. dp2: is palindrome string from index 0 to i */
-        boolean[] dp1 = new boolean[s.length() + 1], dp2 = new boolean[s.length() + 1];
+        int[] min = new int[s.length()];        // min cut at each position of string
+        boolean[][] dp = new boolean[s.length()][s.length()];       // if s(i, j) is palindrome
 
         for (int i = 0; i < s.length(); i++) {
-            minCut[i] = i;      // assume s from 0 to i is not palindrome
-            for (int j = 0; j <= i; j++) {      // from j to i, i is fixed
+            min[i] = i;       // initial value, assume string is not palindrome
 
-                /* j + 1 > i - 1: single char is always palindrome */
-                if (s.charAt(i) == s.charAt(j) && ((j + 1 > i - 1) || dp1[j + 1])) {
-                    dp2[j] = true;
-                    minCut[i] = j == 0 ? 0 : Math.min(minCut[i], minCut[j - 1] + 1);
+            for (int j = 0; j <= i; j++) {      // iter sub string
+                if (s.charAt(j) == s.charAt(i) && (j + 1 > i - 1 || dp[j + 1][i - 1])) {        // j + 1 > i - 1: adjacent char
+                    dp[j][i] = true;
+                    min[i] = j == 0 ? 0 : Math.min(min[i], min[j - 1] + 1);     // j == 0: first char always palindrome
                 }
             }
-            dp1 = dp2;
-            dp2 = new boolean[s.length() + 1];
         }
-
-        return minCut[s.length() - 1];
+        return min[s.length() - 1];
     }
 
     public static void main(String[] args) {
         MinCut_132 t = new MinCut_132();
         System.out.println(t.minCut("abcba"));
+        System.out.println(t.minCut("cabababcbc"));
+
     }
 }
