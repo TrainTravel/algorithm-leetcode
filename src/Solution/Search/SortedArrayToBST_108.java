e0e32a3da60a74f0a7db16e8b26a227082d2125a
Null <borismirage00@gmail.com>
2019-06-11 11:12:49 -0700

    Avoid overflow

--- src/Solution/Search/SortedArrayToBST_108.java
+++ src/Solution/Search/SortedArrayToBST_108.java
@@ -43,7 +43,7 @@ public class SortedArrayToBST_108 {
             return null;
         }
 
-        int mid = (start + end) / 2;
+        int mid = start + (end - start) / 2;        // avoid overflow
 
         TreeNode root = new TreeNode(num[mid]);
 
