c2e1d661a77b07e750650f0d53333f9b4c6d5eb0
Null <borismirage00@gmail.com>
2019-06-11 11:22:07 -0700

    Convert Sorted List to Binary Search Tree

---
+++ src/Solution/Search/SortedListToBST_109.java
@@ -0,0 +1,62 @@
+package Solution.Search;
+
+import Lib.ListNode;
+import Lib.Tree.TreeNode;
+
+/**
+ * Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+ * Height-balanced binary tree is a binary tree that the depth of the two subtrees of node never differ by more than 1.
+ *
+ * @author BorisMirage
+ * Time: 2019/06/11 11:01
+ * Created with IntelliJ IDEA
+ */
+
+public class SortedListToBST_109 {
+    /**
+     * Linked list can not directly get item by index.
+     * Therefore, use two pointers to traverse the linked list, when faster one reaches the end of list, slow one is at root.
+     *
+     * @param head head node
+     * @return root of height balanced BST
+     */
+    public TreeNode sortedListToBST(ListNode head) {
+
+        /* Corner case */
+        if (head == null) {
+            return null;
+        }
+
+        return builder(head, null);
+    }
+
+    /**
+     * Use two pointer to find root node.
+     * Fast node moves two time faster than slow node.
+     * In this way, if fast node reaches the end, slow node will accurately at the mid of list, which is root.
+     *
+     * @param head head node
+     * @param tail end node
+     * @return root of height balanced BST
+     */
+    public TreeNode builder(ListNode head, ListNode tail) {
+
+        ListNode slow = head;       // find root (in the mid of linked list)
+        ListNode fast = head;       // two time faster move speed
+
+        if (head == tail) {
+            return null;
+        }
+
+        while (fast != tail && fast.next != tail) {
+            fast = fast.next.next;      // when fast reaches the end, slow will be at mid of list
+            slow = slow.next;           // stop at root
+        }
+
+        TreeNode root = new TreeNode(slow.val);
+        root.left = builder(head, slow);
+        root.right = builder(slow.next, tail);
+
+        return root;
+    }
+}
