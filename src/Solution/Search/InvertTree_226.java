8c5f2ac9e13026a194843a7e21fb668c8dcf2dbc
Null <borismirage00@gmail.com>
2019-06-11 17:08:44 -0700

    Invert Binary Tree

---
+++ src/Solution/Search/InvertTree_226.java
@@ -0,0 +1,34 @@
+package Solution.Search;
+
+import Lib.Tree.TreeNode;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/06/11 16:48
+ * Created with IntelliJ IDEA
+ */
+
+public class InvertTree_226 {
+    /**
+     * Traverse to bottom to tree and invert child of it.
+     *
+     * @param root root node
+     * @return inverted tree
+     */
+    public TreeNode invertTree(TreeNode root) {
+
+        /* End point */
+        if (root == null) {
+            return root;
+        }
+
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        return root;
+    }
+}
