60460cacf44ddafe275a000535f5c69b77da1580
Null <borismirage00@gmail.com>
2019-06-13 13:50:36 -0700

    Inorder Successor/Predecessor in BST

---
+++ src/Solution/Search/InorderSuccessor_285.java
@@ -0,0 +1,78 @@
+package Solution.Search;
+
+import Lib.Tree.TreeNode;
+
+/**
+ * Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+ * The successor of a node p is the node with the smallest key greater than p.val.
+ * Note:
+ * If the given node has no in-order successor in the tree, return null.
+ * It's guaranteed that the values of the tree are unique.
+ *
+ * @author BorisMirage
+ * Time: 2019/06/13 10:27
+ * Created with IntelliJ IDEA
+ */
+
+public class InorderSuccessor_285 {
+    /**
+     * Successor will be at right sub tree, or previous root, or null.
+     * The target is to find p's closest node (in inorder traversal) and the node's value is larger than p's value.
+     * The in-order traversal in BST is to reveal the ascending order of elements in tree.
+     *
+     * @param root root node
+     * @param p    target node
+     * @return in-order successor in the tree, or null
+     */
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+
+        return successor(root, p);
+    }
+
+    /**
+     * p is the largest element in current tree:
+     * The in-order successor is the parent of current BST’s root.
+     * p is in the right sub tree:
+     * Recursively find a tree that is the right sub tree of p, the in-order successor is the smallest in the subtree.
+     * Or returns previous condition if p is the largest element in BST.
+     * p is in the left sub tree:
+     * Recursively found p in left subtree, and then turned into previous condition.
+     *
+     * @param r current root
+     * @param p target node
+     * @return in-order successor, or null
+     */
+    private TreeNode successor(TreeNode r, TreeNode p) {
+        if (r == null) {
+            return null;
+        }
+
+        if (p.val >= r.val) {
+            return successor(r.right, p);        // rightmost sub tree's successor is at its right sub tree
+        } else {        // p.val < r.val -> left subtree
+
+            TreeNode left = successor(r.left, p);
+            return (left == null) ? r : left;
+        }
+    }
+
+    /**
+     * In-order predecessor of BST.
+     * Idea is almost the same, simply switch the condition, since predecessor is the previous element in BST.
+     *
+     * @param root current root
+     * @param p    target node
+     * @return in-order predecessor, or null
+     */
+    private TreeNode predecessor(TreeNode root, TreeNode p) {
+        if (root == null)
+            return null;
+
+        if (root.val >= p.val) {
+            return predecessor(root.left, p);       // find smaller number
+        } else {
+            TreeNode right = predecessor(root.right, p);
+            return (right != null) ? right : root;
+        }
+    }
+}
