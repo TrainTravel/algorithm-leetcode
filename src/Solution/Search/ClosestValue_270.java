92f14f75315c2e9252b1d9a57692b8a094301cf1
Null <borismirage00@gmail.com>
2019-06-13 10:25:41 -0700

    Closest Binary Search Tree Value

---
+++ src/Solution/Search/ClosestValue_270.java
@@ -0,0 +1,38 @@
+package Solution.Search;
+
+import Lib.Tree.TreeNode;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/06/12 19:18
+ * Created with IntelliJ IDEA
+ */
+
+public class ClosestValue_270 {
+    /**
+     * Traverse of tree until find the closest value in tree.
+     * If the absolute difference between target and node value is less than 0.5, then the closest node is found.
+     * Otherwise, keep searching in BST until the iteration is ended.
+     *
+     * @param root   root node
+     * @param target target double number
+     * @return value in the BST that is closest to the target
+     */
+    public int closestValue(TreeNode root, double target) {
+
+        int remain = root.val;
+
+        while (root != null) {
+
+            if (Math.abs(target - root.val) < Math.abs(target - remain)) {
+                remain = root.val;      // find min value
+
+                if (Math.abs(target - root.val) < 0.5) {
+                    break;      // no other numbers can be closer than 0.5
+                }
+            }
+            root = root.val > target ? root.left : root.right;
+        }
+        return remain;
+    }
+}
