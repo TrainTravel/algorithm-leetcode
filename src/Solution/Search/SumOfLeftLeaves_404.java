07079d187e4c53b95d286d69a406ba51440b52b3
Null <borismirage00@gmail.com>
2019-06-12 18:15:11 -0700

    Sum of Left Leaves

---
+++ src/Solution/Search/SumOfLeftLeaves_404.java
@@ -0,0 +1,52 @@
+package Solution.Search;
+
+import Lib.Tree.TreeNode;
+
+/**
+ * Find the sum of all left leaves in a given binary tree.
+ *
+ * @author BorisMirage
+ * Time: 2019/06/12 17:57
+ * Created with IntelliJ IDEA
+ */
+
+public class SumOfLeftLeaves_404 {
+
+    private int sum = 0;
+
+    /**
+     * DFS.
+     *
+     * @param root root node
+     * @return sum of all left leaves
+     */
+    public int sumOfLeftLeaves(TreeNode root) {
+
+        dfs(root);
+        return sum;
+    }
+
+    /**
+     * DFS to find all leaves, and if current root has a left leaf, add its value to sum.
+     *
+     * @param r root
+     * @return if root is a leaf
+     */
+    private boolean dfs(TreeNode r) {
+
+        /* End point */
+        if (r == null) {
+            return false;
+        }
+
+        if (r.left == null && r.right == null) {
+            return true;        // found a leaf
+        }
+
+        if (dfs(r.left)) {
+            sum += r.left.val;
+        }
+        dfs(r.right);
+        return false;
+    }
+}
