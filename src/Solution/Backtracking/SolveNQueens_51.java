d5a160537119350c80bfea5d9a44f7a8b43c1fd6
Null <borismirage00@gmail.com>
2019-06-17 21:20:59 -0700

    Remove redundant description

--- src/Solution/Backtracking/SolveNQueens_51.java
+++ src/Solution/Backtracking/SolveNQueens_51.java
@@ -9,19 +9,6 @@ import java.util.List;
  * Given an integer n, return all distinct solutions to the n-queens puzzle.
  * Each solution contains a distinct board configuration of the n-queens' placement.
  * 'Q' and '.' both indicate a queen and an empty space respectively.
- * <p>
- * Example: Input: 4
- * Output: [
- * [".Q..",  // Solution 1
- * "...Q",
- * "Q...",
- * "..Q."],
- * <p>
- * ["..Q.",  // Solution 2
- * "Q...",
- * "...Q",
- * ".Q.."]
- * ]
  *
  * @author BorisMirage
  * Time: 2018/07/10 22:26
