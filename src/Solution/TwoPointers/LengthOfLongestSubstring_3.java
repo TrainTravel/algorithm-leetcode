6d8a063a4e654c917cc6d5c25ebc62d72b4b5ae7
Null <borismirage00@gmail.com>
2019-06-18 10:44:56 -0700

    Redo Longest Substring Without Repeating Characters

---
+++ src/Solution/TwoPointers/LengthOfLongestSubstring_3.java
@@ -0,0 +1,59 @@
+package Solution.TwoPointers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * @author BorisMirage
+ * Time: 2018/03/23 12:27
+ * Created with IntelliJ IDEA
+ */
+
+
+public class LengthOfLongestSubstring_3 {
+    /**
+     * Construct a new map to store previous chars.
+     * key - char
+     * value - next position
+     * If current char does not exist in map, then store this char and its next position.
+     * If current char exists in map, reset the position to current char's next. (No duplication)
+     * Compare current non-repeating substring length to max length and find longer one.
+     *
+     * @param s input string
+     * @return max sub-string length
+     */
+    public int lengthOfLongestSubstring(String s) {
+
+        /* Corner case */
+        if (s.length() < 2) {
+            return s.length();
+        }
+
+        int max = 0;
+        Map<Character, Integer> m = new HashMap<>();
+
+        for (int i = 0, j = 0; i < s.length(); i++) {
+
+            if (m.containsKey(s.charAt(i))) {       // current char in map (duplicated)
+                j = Math.max(m.get(s.charAt(i)), j);        // compare current index to previous index
+            }
+            max = Math.max(max, i - j + 1);
+
+            m.put(s.charAt(i), i + 1);      // continue at next char, if char is duplicated, reset start index
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        LengthOfLongestSubstring_3 test = new LengthOfLongestSubstring_3();
+        System.out.println(test.lengthOfLongestSubstring("tmmzuxt"));
+        System.out.println(test.lengthOfLongestSubstring("a"));
+        System.out.println(test.lengthOfLongestSubstring("dvdf"));      // answer: 3
+
+    }
+}
+
+
