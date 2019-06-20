2aab0e42121180cce6d83fc7dbc39d77911c00a9
Null <borismirage00@gmail.com>
2019-06-19 18:24:03 -0700

    Redo Remove Duplicates from Sorted Array

--- src/Solution/TwoPointers/RemoveDuplicates_26.java
+++ src/Solution/TwoPointers/RemoveDuplicates_26.java
@@ -5,16 +5,6 @@ package Solution.TwoPointers;
  * Remove the duplicates in-place such that each element appear only once and return the new length.
  * Do not allocate extra space for another array.
  * It must be done by modifying the input array in-place with O(1) extra memory.
- * <p>
- * Note: Input array is passed in by reference, modification to it will be known to the caller as well.
- * Output sample:
- * // nums is passed in by reference. (i.e., without making a copy)
- * int len = removeElement(nums, val);
- * // any modification to nums in your function would be known by the caller.
- * // using the length returned by your function, it prints the first len elements.
- * for (int i = 0; i < len; i++) {
- * print(nums[i]);
- * }
  *
  * @author BorisMirage
  * Time: 2018/06/13 18:00
@@ -23,29 +13,29 @@ package Solution.TwoPointers;
 
 public class RemoveDuplicates_26 {
     /**
-     * Two int as pointers.
+     * Two pointers.
      * First pointer is the "i" in for loop. It represent the position in traverse.
      * The other pointer points to the last element in array that is non-duplicate.
      * When more than 2 same int is found, the second pointer stop moving until next non-duplicate int is found.
      * Then next element in nums is that non-duplicate int.
      *
      * @param nums input sorted int array
-     * @return length of array that removed duplicate element
+     * @return length of array after removing duplicate elements
      */
     public int removeDuplicates(int[] nums) {
 
         /* Non-duplicate int pointer */
-        int n = 0;
+        int slow = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0 || nums[i] != nums[i - 1]) {
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (fast == 0 || nums[fast] != nums[fast - 1]) {
 
                 /* Switch array elements so that this array can be divided in the end */
-                nums[n++] = nums[i];
+                nums[slow++] = nums[fast];
             }
         }
 //        System.out.println(Arrays.toString(nums));
-        return n;
+        return slow;
     }
 
     public static void main(String[] args) {
