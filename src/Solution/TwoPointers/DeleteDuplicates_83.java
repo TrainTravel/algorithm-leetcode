2602b59fbadb5e7c8a253af8bd46b7da4efbb94e
Null <borismirage00@gmail.com>
2019-06-20 12:33:56 -0700

    Redo Remove Duplicates from Sorted List

---
+++ src/Solution/TwoPointers/DeleteDuplicates_83.java
@@ -0,0 +1,35 @@
+package Solution.TwoPointers;
+
+import Lib.ListNode;
+
+/**
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ *
+ * @author BorisMirage
+ * Time: 2018/08/16 14:20
+ * Created with IntelliJ IDEA
+ */
+
+public class DeleteDuplicates_83 {
+    /**
+     * Move next pointer in current node to next non-duplicated element.
+     *
+     * @param head head ListNode
+     * @return removed head ListNode
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode cache = head;
+
+        while (cache != null && cache.next != null) {
+            if (cache.val == cache.next.val) {      // if duplicated
+
+                cache.next = cache.next.next;       // move next pointer to current's next and try again
+            } else {
+
+                cache = cache.next;     // if not duplicated, move pointer to this node
+            }
+        }
+        return head;
+    }
+}
