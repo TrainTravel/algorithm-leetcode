bc590e62f4af8b52e9d12db33e2839a8b074ac21
Null <borismirage00@gmail.com>
2019-06-20 13:27:55 -0700

    Redo Merge Sorted Array

--- src/Solution/Array/MergeSortedArray_88.java
+++ src/Solution/Array/MergeSortedArray_88.java
@@ -12,25 +12,28 @@ public class MergeSortedArray_88 {
     /**
      * Two pointers.
      *
-     * @param nums1 first array
+     * @param nums1 first array (larger array)
      * @param m     first array's length
      * @param nums2 second array
      * @param n     second array's length
      */
     public void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
 
-        int i = m - 1;      // nums1
-        int j = n - 1;      // nums2
-        int k = m + n - 1;      // num1 extra length
-        while (i >= 0 && j >= 0) {      // two arrays are sorted, so start at the end
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+        int p1 = m - 1;      // nums1 actual length
+        int p2 = n - 1;      // nums2
+        int i = m + n - 1;      // num1 extra space end
+
+        while (p1 > -1 && p2 > -1) {      // two arrays are sorted, so start at the end (largest)
+
+            if (nums1[p1] > nums2[p2]) {        // fill larger one in two arrays
+                nums1[i--] = nums1[p1--];
             } else {
-                nums1[k--] = nums2[j--];
+                nums1[i--] = nums2[p2--];
             }
         }
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+
+        while (p2 > -1) {       // in case any element in nums2 is smaller than any in nums1
+            nums1[i--] = nums2[p2--];
         }
     }
 }
