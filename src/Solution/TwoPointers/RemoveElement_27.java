f99cb81adf420a65292f8301f0ac9526e0ae2f30
Null <borismirage00@gmail.com>
2019-06-19 18:43:15 -0700

    Redo Remove Element

---
+++ src/Solution/TwoPointers/RemoveElement_27.java
@@ -0,0 +1,34 @@
+package Solution.TwoPointers;
+
+/**
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+ * Do not allocate extra space for another array
+ * It must be done by modifying the input array in-place with O(1) extra memory.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ * Note:
+ * 1. Input array is passed in by reference, modification to it will be known to the caller as well.
+ *
+ * @author BorisMirage
+ * Time: 2019/06/19 18:32
+ * Created with IntelliJ IDEA
+ */
+
+public class RemoveElement_27 {
+    public int removeElement(int[] nums, int val) {
+        if (nums.length < 2) {
+            return nums.length;
+        }
+
+        int slow = 0, fast = 0;
+
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                fast++;
+            } else {
+                nums[slow++] = nums[fast++];
+            }
+        }
+
+        return slow;
+    }
+}
