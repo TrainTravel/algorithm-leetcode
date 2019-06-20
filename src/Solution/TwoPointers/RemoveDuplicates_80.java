80bda923ee92eadc1954ac74199c8ae8239b8cef
Null <borismirage00@gmail.com>
2019-06-20 12:25:07 -0700

    Redo Remove Duplicates from Sorted Array II

--- src/Solution/TwoPointers/RemoveDuplicates_80.java
+++ src/Solution/TwoPointers/RemoveDuplicates_80.java
@@ -5,30 +5,13 @@ package Solution.TwoPointers;
  * Duplicates appeared at most twice and return the new length.
  * Do not allocate extra space for another array.
  * It must be done by modifying the input array in-place with O(1) extra memory.
- * <p>
- * Example 1:
- * Given nums = [1,1,1,2,2,3],
- * Function should return length = 5, with nums being 1, 1, 2, 2 and 3 respectively.
- * It doesn't matter what you leave beyond the returned length.
- * Example 2:
- * Given nums = [0,0,1,1,1,1,2,3,3],
- * Function should return length = 7, with nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
- * It doesn't matter what values are set beyond the returned length.
- * <p>
- * // nums is passed in by reference. (i.e., without making a copy)
- * int len = removeDuplicates(nums);
- * // any modification to nums in your function would be known by the caller.
- * // using the length returned by your function, it prints the first len elements.
- * for (int i = 0; i < len; i++) {
- * print(nums[i]);
- * }
  *
  * @author BorisMirage
  * Time: 2018/08/14 13:49
  * Created with IntelliJ IDEA
  */
 
-public class RemoveDuplicatesII_80 {
+public class RemoveDuplicates_80 {
     /**
      * Similar to RemoveDuplicates_26, simply modify pointer moving condition.
      * Two int as pointers.
@@ -40,13 +23,13 @@ public class RemoveDuplicatesII_80 {
      * @param nums input sorted int array
      * @return length of array that removed duplicate element
      */
-    public int removeDuplicatesII(int[] nums) {
+    public int removeDuplicates(int[] nums) {
 
         /* Non-duplicate int pointer */
         int n = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (i < 2 || nums[i] > nums[n - 2]) {
+            if (i < 2 || nums[i] > nums[n - 2]) {       // the array is sorted
                 nums[n++] = nums[i];
             }
         }
