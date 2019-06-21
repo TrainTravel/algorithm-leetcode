df4208f3b78d170c4f2f54335efa90d37b24d7a7
Null <borismirage00@gmail.com>
2019-06-20 18:04:30 -0700

    Intersection of Two Linked Lists

---
+++ src/Solution/TwoPointers/GetIntersectionNode_160.java
@@ -0,0 +1,37 @@
+package Solution.TwoPointers;
+
+import Lib.ListNode;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/06/20 16:55
+ * Created with IntelliJ IDEA
+ */
+
+public class GetIntersectionNode_160 {
+    /**
+     * Two pointers. Each pointer starts at the beginning of A/B.
+     * When traverse to the end of each list, swap to the other list.
+     * If there is an intersection, these two pointers will meet there.
+     *
+     * @param headA first list head
+     * @param headB second list head
+     * @return intersection node
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        /* Corner case */
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode p1 = headA, p2 = headB;
+
+        while (p1 != p2) {
+            p1 = (p1 != null) ? p1.next : headB;        // if reaches the end, move to next list
+            p2 = (p2 != null) ? p2.next : headA;        // if reaches the end, move to next list
+        }
+
+        return p1;
+    }
+}
