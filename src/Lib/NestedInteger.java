0ec498bd3da0a8d32a5a979e6b81ebda76a4a720
Null <borismirage00@gmail.com>
2019-06-26 17:14:44 -0700

    Revise doc on class

--- src/Lib/NestedInteger.java
+++ src/Lib/NestedInteger.java
@@ -7,25 +7,36 @@ import java.util.List;
  * Time: 2019/05/30 15:01
  * Created with IntelliJ IDEA
  */
-
-
 public interface NestedInteger {
 
-
-    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    /**
+     * @return true if this NestedInteger holds a single integer, rather than a nested list.
+     */
     public boolean isInteger();
 
-    // @return the single integer that this NestedInteger holds, if it holds a single integer
-    // Return null if this NestedInteger holds a nested list
+
+    /**
+     * @return the single integer that this NestedInteger holds, if it holds a single integer. Return null if this NestedInteger holds a nested list
+     */
     public Integer getInteger();
 
-    // Set this NestedInteger to hold a single integer.
+    /**
+     * Set this NestedInteger to hold a single integer.
+     *
+     * @param value value for NestedInteger to hold
+     */
     public void setInteger(int value);
 
-    // Set this NestedInteger to hold a nested list and adds a nested integer to it.
-    public void add(Lib.NestedInteger ni);
+    /**
+     * Set this NestedInteger to hold a nested list and adds a nested integer to it.
+     *
+     * @param ni new NestedInteger
+     */
+    public void add(NestedInteger ni);
 
-    // @return the nested list that this NestedInteger holds, if it holds a nested list
-    // Return null if this NestedInteger holds a single integer
+
+    /**
+     * @return the nested list that this NestedInteger holds, if it holds a nested list. Return null if this NestedInteger holds a single integer
+     */
     public List<Lib.NestedInteger> getList();
 }
