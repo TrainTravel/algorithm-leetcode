47dc9df1903c52390721b415f0b9bfab88992cc2
Null <borismirage00@gmail.com>
2019-06-27 18:34:47 -0700

    Redo Largest Rectangle in Histogram

--- src/Solution/Stack/LargestRectangleArea_84.java
+++ src/Solution/Stack/LargestRectangleArea_84.java
@@ -12,45 +12,48 @@ import java.util.Stack;
  */
 
 public class LargestRectangleArea_84 {
-
     /**
      * Faster solution with stack.
-     * Traverse all elements in array.
-     * Push elements in stack if it is increasing order.
-     * If not, pop top of stack and regard current index position as smallest bar. Calculate area.
-     * After each calculation, move index -1, since there is a possibility that current top is larger than smallest.
+     * Traverse all elements in array and find each increasing subarray.
+     * If increasing subarray is ended, then calculate rectangle size starts from largest element in subarray.
+     * The initially length of rectangle is 1 (itself).
+     * With the pop of stack, length increased by 1 each time until value at top of stack is smaller than current element.
+     * For each deceasing area, simply count the area with itself and compared with max.
+     * Note that if stack is empty, current element is the smallest element in current subarray.
+     * The area length of this rectangle is [0,i].
      *
      * @param heights input histogram's bar height array
      * @return area of largest rectangle
      */
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> temp = new Stack<>();
-        int maxArea = 0;
-        int h;
+
+        Stack<Integer> stack = new Stack<>();       // store index of array
+        int max = 0;
 
         for (int i = 0; i <= heights.length; i++) {
 
-            /* Set each calculation's bar height. If out of bound, set 0 to pop all elements in stack. */
-            h = (i == heights.length ? 0 : heights[i]);
-            if (temp.empty() || h > heights[temp.peek()]) {
+            int h = (i == heights.length) ? 0 : heights[i];
 
-                /* Push index into stack to record position */
-                temp.push(i);
+            while (!stack.isEmpty() && h < heights[stack.peek()]) {      // increasing subarray stopped
 
-            } else {
-                int top = temp.pop();
-                maxArea = Math.max(maxArea, heights[top] * (temp.empty() ? i : i - 1 - temp.peek()));
-
-                /* i - 1 so that the highest bar can be pushed to the end of stack */
-                i--;
+                /*
+                 * Stack only store index, therefore, heights[s.pop()] stands for the length of current height.
+                 * The other part of area calculation is the length of rectangle.
+                 * The length is between current bar and the max bar in increasing subarray.
+                 * If stack is empty, then current element is the smallest element in [0,i], and length will be i.
+                 * i - 1 is to move i to previous max value in previous increasing subarray.
+                 * If stack is empty, current element is the smallest element in current subarray.
+                 * The area length of this rectangle is [0,i]. */
+                int area = heights[stack.pop()] * (i - 1 - (stack.isEmpty() ? -1 : stack.peek()));
+                max = Math.max(max, area);
             }
+            stack.push(i);
         }
-
-        return maxArea;
+        return max;
     }
 
     /**
-     * Slow yet plain solution.
+     * Slow yet plain brute force solution.
      * Traverse all elements in array.
      * In each round, find all elements that is larger than current bar. Calculate max area based on current bar.
      *
@@ -63,7 +66,6 @@ public class LargestRectangleArea_84 {
             return 0;
         }
 
-        /* Set min area */
         int maxArea = heights[0];
 
         for (int i = 0; i < heights.length; i++) {
@@ -88,7 +90,7 @@ public class LargestRectangleArea_84 {
 
     public static void main(String[] args) {
         LargestRectangleArea_84 largestRectangleAreaTest = new LargestRectangleArea_84();
-        int[] arr = {2, 1, 5, 6, 2, 3};
+        int[] arr = {10000, 2, 3, 4, 5};
         System.out.println(largestRectangleAreaTest.largestRectangleArea(arr));
     }
 
