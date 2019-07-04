ec3847ca6e5e7177b91f18d2161ac8827766677c
Null <borismirage00@gmail.com>
2019-07-04 15:30:34 -0700

    Revise comment

--- src/Solution/Others/MultiplyString_43.java
+++ src/Solution/Others/MultiplyString_43.java
@@ -1,6 +1,14 @@
 package Solution.Others;
 
 /**
+ * Given two non-negative integers num1 and num2 represented as strings.
+ * Return the product of num1 and num2, also represented as a string.
+ * Note:
+ * 1. The length of both num1 and num2 is < 110.
+ * 2. Both num1 and num2 contain only digits `0-9`.
+ * 3. Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+ * 4. Do not use any built-in BigInteger library or convert the inputs to integer directly.
+ *
  * @author BorisMirage
  * Time: 2018/06/24 14:16
  * Created with IntelliJ IDEA
@@ -8,9 +16,6 @@ package Solution.Others;
 
 public class MultiplyString_43 {
     /**
-     * Given two non-negative integers num1 and num2 represented as strings.
-     * Return the product of num1 and num2, also represented as a string.
-     * <p>
      * Divide multiply into steps, it can be regarded as one digit multi one digit in each time, finally sum all result.
      * Hence, simply apply this process.
      *
@@ -25,16 +30,15 @@ public class MultiplyString_43 {
             return "0";
         }
 
-        /* Store each result's position. All elements will be combined as a string for string output */
+        /* Store each result's position.
+         * All elements will be combined as a string for string output */
         int[] index = new int[num1.length() + num2.length()];
 
-        /* Traverse each digit char in nums1 and nums2 */
         for (int i = num1.length() - 1; i >= 0; i--) {
             for (int j = num2.length() - 1; j >= 0; j--) {
                 int currentMultiply = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
 
-                /* Add nums that has been current position through previous multiply */
-                int sum = currentMultiply + index[i + j + 1];
+                int sum = currentMultiply + index[i + j + 1];       // add nums through previous calculation
 
                 /* Put basic and tens into correct position */
                 index[i + j] += sum / 10;
@@ -42,10 +46,9 @@ public class MultiplyString_43 {
             }
         }
 
-        /* Traverse all elements in array and construct result string */
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < index.length; i++) {
-            if (!(i == 0 && index[i] == 0)) {
+            if (!(i == 0 && index[i] == 0)) {       // avoid leading 0
                 res.append(index[i]);
             }
         }
