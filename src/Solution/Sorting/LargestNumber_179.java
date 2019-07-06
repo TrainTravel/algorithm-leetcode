7ca130c6de3daaa8e1cfe88155108c56ce41c3c5
Null <borismirage00@gmail.com>
2019-07-06 16:20:42 -0700

    Largest Number

---
+++ src/Solution/Sorting/LargestNumber_179.java
@@ -0,0 +1,55 @@
+package Solution.Sorting;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/07/06 15:56
+ * Created with IntelliJ IDEA
+ */
+
+public class LargestNumber_179 {
+    /**
+     * The principle of comparison is similar to dictionary lexical order.
+     * Only compare first number between two numbers, place larger one at head of result for a largest number.
+     * If two numbers have same first digit, then compare second digit with same rule.
+     *
+     * @param nums given array
+     * @return largest # that this array could make up
+     */
+    public String largestNumber(int[] nums) {
+
+        /* Corner case */
+        if (nums == null || nums.length == 0) {
+            return "";
+        }
+
+        String[] num = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            num[i] = String.valueOf(nums[i]);       // convert int to string for comparator
+        }
+
+        /* The principle of comparison is similar to dictionary lexical order.
+         * Only compare first number between two numbers, place larger one at head of result for a largest number.
+         * If two numbers have same first digit, then compare second digit with same rule. */
+        Comparator<String> comparator = (str1, str2) -> {
+            String s1 = str1 + str2;
+            String s2 = str2 + str1;
+            return s2.compareTo(s1);
+        };
+
+        Arrays.sort(num, comparator);
+
+        if (num[0].charAt(0) == '0') {      // if whole array is 0
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : num) {
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+}
