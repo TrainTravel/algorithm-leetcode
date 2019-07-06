fec3d3be7f6858854fd2d1e7f933e313635f6d8e
Null <borismirage00@gmail.com>
2019-07-05 20:41:22 -0700

    Revise comment

--- src/Solution/TwoPointers/Trap_42.java
+++ src/Solution/TwoPointers/Trap_42.java
@@ -1,6 +1,5 @@
 package Solution.TwoPointers;
 
-
 /**
  * Given n non-negative integers representing an elevation map where the width of each bar is 1.
  * Compute how much water it is able to trap after raining.
@@ -15,7 +14,7 @@ public class Trap_42 {
      * Use two pointers as left start pointer and right start pointer.
      * Move pointer toward center in iteration and add valid value to result.
      *
-     * @param height int array that store map
+     * @param height int array of elevation map
      * @return trap space
      */
     public int trap(int[] height) {
@@ -26,55 +25,55 @@ public class Trap_42 {
         }
 
         int res = 0;
-        int indexLeft = 0;
-        int indexRight = height.length - 1;
+        int left = 0;
+        int right = height.length - 1;
 
-        /* Find correct start and end bound */
-        while (indexLeft < indexRight && height[indexLeft] < height[indexLeft + 1]) {
-            indexLeft++;
+        /* Find correct start and end bound
+         * From left to right, find end of increasing order.
+         * From right to left, find the end of decreasing order. */
+        while (left < right && height[left] < height[left + 1]) {
+            left++;
         }
-        while (indexLeft < indexRight && height[indexRight] < height[indexRight - 1]) {
-            indexRight--;
+        while (left < right && height[right] < height[right - 1]) {
+            right--;
         }
 
-        /* If no valid "pit" can be found */
-        if (indexLeft >= indexRight) {
+        if (left >= right) {        // if no valid "pit" can be found
             return 0;
         }
 
-        /* Traverse array to find all "pit" */
-        while (indexLeft < indexRight) {
+        while (left < right) {
 
-            /* Obtain each value in array. If valid, add to result. */
-            int leftValue = height[indexLeft];
-            int rightValue = height[indexRight];
+            /* Obtain each value in array.
+             * If valid, add to result. */
+            int leftValue = height[left];
+            int rightValue = height[right];
 
-            /* If left is smaller than right, then move left pointer since container is depended by smaller height */
+            /* If left is smaller than right, then move left pointer.
+             * Container is depended by smaller height */
             if (leftValue < rightValue) {
 
                 /* Note: height[indexLeft + 1] can be replaced as height[++indexLeft].
                  * And the indexLeft++ needs to be removed.
                  * But only ++left rather than anything else.
                  * The reason is that ++indexLeft increments the number before the current expression is evaluated */
-                while (indexLeft < indexRight && leftValue > height[indexLeft + 1]) {
-                    res += leftValue - height[indexLeft + 1];
-                    indexLeft++;
+                while (left < right && leftValue > height[left + 1]) {
+                    res += leftValue - height[left + 1];
+                    left++;
                 }
-                indexLeft++;
+                left++;
             } else {
-                while (indexLeft < indexRight && rightValue > height[indexRight - 1]) {
-                    res += rightValue - height[indexRight - 1];
-                    indexRight--;
+                while (left < right && rightValue > height[right - 1]) {
+                    res += rightValue - height[right - 1];
+                    right--;
                 }
-                indexRight--;
+                right--;
             }
         }
         return res;
     }
 
     public static void main(String[] args) {
-
-        /* Trapping Rain Water */
         Trap_42 trapTest = new Trap_42();
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         System.out.println(trapTest.trap(height));
