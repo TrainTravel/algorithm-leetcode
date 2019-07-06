a4f123bcde2a2889fb4cc00e3943b08754470bdd
Null <borismirage00@gmail.com>
2019-07-05 18:08:31 -0700

    First Missing Positive

--- src/Solution/Others/FirstMissingPositive_41.java
+++ src/Solution/Others/FirstMissingPositive_41.java
@@ -13,10 +13,11 @@ package Solution.Others;
 public class FirstMissingPositive_41 {
     /**
      * Traverse array three times.
+     * Smallest missing positive integer can only be in [1, n + 1] (all inclusive).
      * First, replace all non-positive numbers to array's length.
-     * In second traversal, find nums[i] that absolute value is smaller than array's length.
+     * In second traversal, find nums[i] such that its absolute value is smaller than array's length.
      * Mark its representative index's value to negative.
-     * i.e, abs(nums[i])=5, then nums[4] = - nums[4]
+     * i.e, abs(nums[i]) = 5, then nums[4] = - nums[4]
      * The reason to use absolute value is to save while loop compare to swapping method.
      *
      * @param nums input int array
@@ -29,49 +30,35 @@ public class FirstMissingPositive_41 {
             return 1;
         }
 
-        /* Remove all negative elements */
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] <= 0) {
-                nums[i] = nums.length + 1;
-            }
+            nums[i] = (nums[i] < 1) ? nums.length + 1 : nums[i];      // replace all negative value to n + 1
         }
 
-        /* Mark each element that is smaller than length of array to negative value
-         * To avoid unnecessary move in swapping, use absolute value and mark respective position in negative number */
-        for (int i = 0; i < nums.length; ++i) {
+        for (int i = 0; i < nums.length; i++) {
             if (Math.abs(nums[i]) <= nums.length) {
-                nums[Math.abs(nums[i]) - 1] = -Math.abs(nums[Math.abs(nums[i]) - 1]);
+                nums[Math.abs(nums[i]) - 1] = -Math.abs(nums[Math.abs(nums[i]) - 1]);       // mark index as "presented"
             }
         }
 
-        /* Finally, traverse array and return first positive index, which is first missing positive number */
-        for (int i = 0; i < nums.length; ++i) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) {
                 return i + 1;
             }
         }
+
         return nums.length + 1;
     }
 
     public static void main(String[] args) {
 
-        /* First Missing Positive */
         FirstMissingPositive_41 firstMissingPositiveTest = new FirstMissingPositive_41();
-        int[] c = {2, 3, 6, 7};
-        System.out.println(firstMissingPositiveTest.firstMissingPositive(c));       // 1
-        c = new int[]{3, 4, -1, 1};
-        System.out.println(firstMissingPositiveTest.firstMissingPositive(c));       // 2
-        c = new int[]{7, 8, 9, 11, 12};
-        System.out.println(firstMissingPositiveTest.firstMissingPositive(c));       // 1
-        c = new int[]{1, 2, 0};
-        System.out.println(firstMissingPositiveTest.firstMissingPositive(c));       // 3
-        c = new int[]{2, 5, 2, 1, 2, 3};
-        System.out.println(firstMissingPositiveTest.firstMissingPositive(c));       // 4
-        c = new int[]{3, 4, 0, 1};
-        System.out.println(firstMissingPositiveTest.firstMissingPositive(c));       // 2
-        c = new int[]{1, 2, 3, 4, 5, 6, 7};
-        System.out.println(firstMissingPositiveTest.firstMissingPositive(c));       // 8
-        c = new int[]{7, 10, 12, 11, 9, 6, 5, 1, 2, 3};
-        System.out.println(firstMissingPositiveTest.firstMissingPositive(c));       // 4
+        System.out.println(firstMissingPositiveTest.firstMissingPositive(new int[]{2, 3, 6, 7}));       // 1
+        System.out.println(firstMissingPositiveTest.firstMissingPositive(new int[]{3, 4, -1, 1}));       // 2
+        System.out.println(firstMissingPositiveTest.firstMissingPositive(new int[]{7, 8, 9, 11, 12}));       // 1
+        System.out.println(firstMissingPositiveTest.firstMissingPositive(new int[]{1, 2, 0}));       // 3
+        System.out.println(firstMissingPositiveTest.firstMissingPositive(new int[]{2, 5, 2, 1, 2, 3}));       // 4
+        System.out.println(firstMissingPositiveTest.firstMissingPositive(new int[]{3, 4, 0, 1}));       // 2
+        System.out.println(firstMissingPositiveTest.firstMissingPositive(new int[]{1, 2, 3, 4, 5, 6, 7}));       // 8
+        System.out.println(firstMissingPositiveTest.firstMissingPositive(new int[]{7, 10, 12, 11, 9, 6, 5, 1, 2, 3}));       // 4
     }
 }
