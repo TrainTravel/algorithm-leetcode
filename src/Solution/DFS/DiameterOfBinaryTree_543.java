10b3b9e5acb9863aad25395964ce5edd020a8f03
Null <borismirage00@gmail.com>
2019-07-13 15:55:21 -0700

    Diameter of Binary Tree

---
+++ src/Solution/DFS/DiameterOfBinaryTree_543.java
@@ -0,0 +1,53 @@
+package Solution.DFS;
+
+import Lib.Tree.TreeNode;
+
+/**
+ * Given a binary tree, you need to compute the length of the diameter of the tree.
+ * The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
+ * This path may or may not pass through the root.
+ *
+ * @author BorisMirage
+ * Time: 2019/07/13 15:44
+ * Created with IntelliJ IDEA
+ */
+
+public class DiameterOfBinaryTree_543 {
+    private int max = 0;        // global max
+
+    /**
+     * Find max depth in both left sub tree and right subtree.
+     *
+     * @param root root node
+     * @return max path
+     */
+    public int diameterOfBinaryTree(TreeNode root) {
+
+        /* Corner case */
+        if (root == null) {
+            return 0;
+        }
+        path(root);
+        return max;
+    }
+
+    /**
+     * Find current tree node max depth.
+     *
+     * @param root root node
+     * @return max depth of current node
+     */
+    private int path(TreeNode root) {
+
+        /* End point */
+        if (root == null) {
+            return 0;
+        }
+
+        int left = path(root.left);
+        int right = path(root.right);
+        max = Math.max(max, left + right);      // global max
+
+        return Math.max(left, right) + 1;       // current max
+    }
+}
