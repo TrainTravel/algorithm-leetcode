151137481e3c133faab785f395ce49e5ec4b67d2
Null <borismirage00@gmail.com>
2019-07-15 13:27:31 -0700

    Use iteration instead of using recursion

--- src/Solution/ListNode/MergeTwoSortedLists_21.java
+++ src/Solution/ListNode/MergeTwoSortedLists_21.java
@@ -21,18 +21,20 @@ public class MergeTwoSortedLists_21 {
      * @return next smaller node
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (l1 != null || l2 != null) {
+            if (l1 == null || (l2 != null && l1.val >= l2.val)) {
+                current.next = l2;
+                current = l2;
+                l2 = l2.next;
+            } else {
+                current.next = l1;
+                current = l1;
+                l1 = l1.next;
+            }
         }
-        if (l2 == null) {
-            return l1;
-        }
-        if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
-        }
+        return dummy.next;
     }
 }
