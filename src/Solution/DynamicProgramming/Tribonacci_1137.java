c063160625fcb60d1047e6552477a12e2b7e4fd4
Null <borismirage00@gmail.com>
2019-07-27 21:45:18 -0700

    N-th Tribonacci Number

---
+++ src/Solution/DynamicProgramming/Tribonacci_1137.java
@@ -0,0 +1,37 @@
+package Solution.DynamicProgramming;
+
+/**
+ * The Tribonacci sequence Tn is defined as follows:
+ * T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+ * Given n, return the value of Tn.
+ *
+ * @author BorisMirage
+ * Time: 2019/07/27 21:22
+ * Created with IntelliJ IDEA
+ */
+
+public class Tribonacci_1137 {
+    /**
+     * Basic dynamic programming, dp[i] = dp[i-1] + dp[i-2] + dp[i-3].
+     *
+     * @param n n
+     * @return nth Tribonacci sequence
+     */
+    public int tribonacci(int n) {
+
+        /* Corner case */
+        if (n < 3) {
+            return (n == 0) ? 0 : 1;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        return dp[n];
+    }
+}
