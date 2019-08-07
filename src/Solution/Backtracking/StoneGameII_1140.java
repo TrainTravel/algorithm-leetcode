3922a9102763172faba918b35a713bee9af3e404
Null <borismirage00@gmail.com>
2019-08-07 14:55:37 -0700

    Stone Game II

--- src/Solution/Backtracking/StoneGameII_1140.java
+++ src/Solution/Backtracking/StoneGameII_1140.java
@@ -6,7 +6,7 @@ package Solution.Backtracking;
  * The objective of the game is to end with the most stones.
  * Alex and Lee take turns, with Alex starting first.  Initially, M = 1.
  * On each player's turn, that player can take all the stones in the first X remaining piles, where 1 <= X <= 2M.
- * Then, we set M = max(M, X).
+ * Then, set M = max(M, X).
  * The game continues until all the stones have been taken.
  * Assuming Alex and Lee play optimally, return the maximum number of stones Alex can get.
  *
