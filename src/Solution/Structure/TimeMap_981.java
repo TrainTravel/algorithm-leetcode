15c87bca54e573471b42a52404a14dad3b671cac
Null <borismirage00@gmail.com>
2019-08-14 18:56:02 -0700

    Add problem description

--- src/Solution/Structure/TimeMap_981.java
+++ src/Solution/Structure/TimeMap_981.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 import java.util.TreeMap;
 
 /**
+ * Create a timebased key-value store class TimeMap, that supports two operations.
+ * 1. set(string key, string value, int timestamp): Stores the key and value, along with the given timestamp.
+ * 2. get(string key, int timestamp): Returns a value such that set(key, value, timestamp_prev) was called previously, with timestamp_prev <= timestamp.
+ * If there are multiple such values, it returns the one with the largest timestamp_prev.
+ * If there are no values, it returns the empty string ("").
+ *
  * @author BorisMirage
  * Time: 2019/07/27 15:03
  * Created with IntelliJ IDEA
