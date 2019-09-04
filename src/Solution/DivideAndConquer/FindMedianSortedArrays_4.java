e591ad18e2fcbbc12e850210c96955104b63a0e9
Null <borismirage00@gmail.com>
2019-09-04 14:51:38 -0700

    Move to correct category

--- src/Solution/DivideAndConquer/FindMedianSortedArrays_4.java
+++ src/Solution/DivideAndConquer/FindMedianSortedArrays_4.java
@@ -1,4 +1,4 @@
-package Solution.BinarySearch;
+package Solution.DivideAndConquer;
 
 /**
  * There are two sorted arrays nums1 and nums2 of size m and n respectively, find the median of the two sorted arrays.
