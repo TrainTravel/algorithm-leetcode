6b7394e74e22dff0fac062080483637024e60783
Null <borismirage00@gmail.com>
2019-09-29 14:44:49 -0700

    Another approach: DFS

---
+++ src/Playground/Test.java
@@ -0,0 +1,16 @@
+package Playground;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/09/28 16:00
+ * Created with IntelliJ IDEA
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> l = new LinkedList<>();
+    }
+}
--- src/Solution/DynamicProgramming/IsMatch_10.java
+++ src/Solution/DynamicProgramming/IsMatch_10.java
@@ -1,5 +1,8 @@
 package Solution.DynamicProgramming;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
  * '.' Matches any single character.
@@ -89,21 +92,94 @@ public class IsMatch_10 {
         return dp[l1][l2];
     }
 
+    /**
+     * Use DFS searching to find if s is matched to p.
+     * During the searching process, use a 2D int array to record previous result as pruning.
+     *
+     * @param s string
+     * @param p pattern string
+     * @return if string is matched to pattern
+     */
+    public boolean dfsImpl(String s, String p) {
+
+        /* Corner case */
+        if (p == null || s == null) {
+            return false;
+        }
+        int[][] mem = new int[s.length() + 1][p.length() + 1];
+
+        for (int[] i : mem) {
+            Arrays.fill(i, -1);
+        }
+
+        for (int i = p.length(); i >= 0; i -= 2) {
+            if (i == p.length() || (i < p.length() && p.charAt(i + 1) == '*')) {
+                mem[s.length()][i] = 1;
+            } else {
+                break;
+            }
+        }
+
+        return dfs(s, p, 0, 0, mem);
+    }
+
+    /**
+     * DFS searching with a 2D int table to accomplish pruning (can be done by hash map).
+     *
+     * @param s   string
+     * @param p   pattern string
+     * @param i   current index of string
+     * @param j   current index of pattern
+     * @param mem 2D boolean array to store the previous result
+     * @return if string is matched to pattern
+     */
+    private boolean dfs(String s, String p, int i, int j, int[][] mem) {
+
+        if (mem[i][j] != -1) {      // reuse previous result
+            return mem[i][j] == 1;
+        }
+
+        if (j >= p.length()) {      // avoid overflow
+            return false;
+        }
+
+        if (p.charAt(j) == '*') {
+            int n = i - 2;
+
+            /*
+             * n - 2: char in pattern before * and one char ahead. This is to handle the case where * matches 0 times.
+             * 1. If '*' is the second char in pattern: n == i - 2
+             * 2. If '*' matches 0 times in s
+             * If char before '*' is '.', then it can match for any char for any times: p.charAt(j - 1) == '.' */
+            while (n < s.length() && (n == i - 2 || s.charAt(n) == p.charAt(j - 1) || p.charAt(j - 1) == '.')) {
+                if (dfs(s, p, ++n, j + 1, mem)) {
+                    mem[i][j] = 1;
+                    return true;
+                }
+            }
+        } else if (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) {
+            return dfs(s, p, i + 1, j + 1, mem);
+        } else if (j + 1 < p.length() && p.charAt(j + 1) == '*') {      // match previous char more than 1 time
+            return dfs(s, p, i, j + 2, mem);
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
+
         IsMatch_10 test = new IsMatch_10();
         String s = "ab";
         String p = ".*";
-        System.out.println(test.isMatch(s, p));
+        System.out.println(test.dfsImpl(s, p));     // T
 
-//        test = new IsMatch_10();
-//        s = "mississippi";
-//        p = "mis*is*p*.";
-//        System.out.println(test.isMatch(s, p));
+        s = "mississippi";
+        p = "mis*is*p*.";
+        System.out.println(test.dfsImpl(s, p));     // F
 
-        test = new IsMatch_10();
         s = "aasdfasdfasdfasdfas";
         p = "aasdf.*asdf.*asdf.*asdf.*s";
-        System.out.println(test.isMatch(s, p));
+        System.out.println(test.dfsImpl(s, p));     // T
     }
 
 }
