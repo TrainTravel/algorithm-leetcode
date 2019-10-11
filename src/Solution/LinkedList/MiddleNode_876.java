5ef15eb68ba07ec0989e28ce2af2637d9bf9e84c
Null <borismirage00@gmail.com>
2019-10-11 16:36:41 -0700

    Middle of the Linked List

---
+++ src/Solution/LinkedList/MiddleNode_876.java
@@ -0,0 +1,31 @@
+package Solution.LinkedList;
+
+import Lib.ListNode;
+
+/**
+ * Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+ * If there are two middle nodes, return the second middle node.
+ *
+ * @author BorisMirage
+ * Time: 2019/10/11 16:32
+ * Created with IntelliJ IDEA
+ */
+
+public class MiddleNode_876 {
+    /**
+     * Two pointers. Fast one and slow one.
+     *
+     * @param head head of list
+     * @return middle of list
+     */
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
