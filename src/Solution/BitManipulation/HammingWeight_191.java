817cfd8e4d587f9124bd10814444767022ec14c2
Null <borismirage00@gmail.com>
2019-10-11 20:14:18 -0700

    Number of 1 Bits

---
+++ src/Solution/BitManipulation/HammingWeight_191.java
@@ -0,0 +1,27 @@
+package Solution.BitManipulation;
+
+/**
+ * Write a function that takes an unsigned integer, return the number of '1' bits it has (also known as the Hamming weight).
+ *
+ * @author BorisMirage
+ * Time: 2019/10/11 20:12
+ * Created with IntelliJ IDEA
+ */
+
+public class HammingWeight_191 {
+    /**
+     * Right shift one bit each time.
+     *
+     * @param n given int
+     * @return bit in int
+     */
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n > 0) {
+            count = count + (1 & n);
+            n >>>= 1;
+        }
+
+        return count;
+    }
+}
