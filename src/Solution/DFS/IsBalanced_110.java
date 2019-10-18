cb7122987f5c60ee4eeed1675f99f1f89bdaf7ff
Null <borismirage00@gmail.com>
2019-10-18 15:25:52 -0700

    Add bottom-up DFS

--- src/Solution/DFS/IsBalanced_110.java
+++ src/Solution/DFS/IsBalanced_110.java
@@ -14,7 +14,8 @@ import Lib.Tree.TreeNode;
 
 public class IsBalanced_110 {
     /**
-     * DFS traversal.
+     * DFS top-bottom traversal.
+     * Time complexity: O(nlogn), each time, the left subtree and right subtree is called in recursion.
      *
      * @param root root node
      * @return if given tree is height-balanced
@@ -47,5 +48,68 @@ public class IsBalanced_110 {
         }
 
         return Math.max(left, right) + 1;       // depth + 1
+    }
+
+    /**
+     * The bottom-up approach is a reverse of the logic of the top-down approach.
+     * Traverse to the bottom of the tree, then compare the height between the height of each subtree.
+     * In this way, the duplicate calculation occurred in top-bottom traversal can be reduced.
+     * Time complexity: O(n).
+     * However, it requires a additional class to record the height of tree.
+     *
+     * @param root root node
+     * @return if given tree is height-balanced
+     */
+    public boolean bottomUp(TreeNode root) {
+
+        /* Corner case */
+        if (root == null) {
+            return true;
+        }
+        if (root.left == null && root.right == null) {
+            return true;
+        }
+
+        return helper(root, 1).isBalanced;
+    }
+
+    /**
+     * Bottom-up traversal to find if left subtree and right subtree is balanced.
+     *
+     * @param r      current root node
+     * @param height current tree node's height
+     * @return if given tree is height-balanced
+     */
+    private TreeNodeWithHeight helper(TreeNode r, int height) {
+        if (r == null) {
+            return new TreeNodeWithHeight(-1, true);
+        }
+
+        TreeNodeWithHeight left = helper(r.left, height + 1);
+        TreeNodeWithHeight right = helper(r.right, height + 1);
+        if (!left.isBalanced || !right.isBalanced || Math.abs(left.height - right.height) > 1) {
+            return new TreeNodeWithHeight(-1, false);
+        }
+
+        return new TreeNodeWithHeight(Math.max(left.height, right.height) + 1, true);
+    }
+
+    /**
+     * Modified tree node with current node height.
+     */
+    static class TreeNodeWithHeight {
+        boolean isBalanced;
+        int height;
+
+        /**
+         * Initialization of node
+         *
+         * @param height     height of current node
+         * @param isBalanced is current tree balanced
+         */
+        TreeNodeWithHeight(int height, boolean isBalanced) {
+            this.height = height;
+            this.isBalanced = isBalanced;
+        }
     }
 }
