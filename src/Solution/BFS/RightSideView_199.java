5926533dc8dbbaf1737f5950261ea36d8413b7f8
Null <borismirage00@gmail.com>
2019-10-28 16:32:14 -0700

    Binary Tree Right Side View

---
+++ src/Solution/BFS/RightSideView_199.java
@@ -0,0 +1,89 @@
+package Solution.BFS;
+
+import Lib.Tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, imagine yourself standing on the right side of it.
+ * Return the values of the nodes you can see ordered from top to bottom.
+ *
+ * @author BorisMirage
+ * Time: 2019/10/28 12:22
+ * Created with IntelliJ IDEA
+ */
+
+public class RightSideView_199 {
+    /**
+     * @param root root node
+     * @return values of the nodes you can see ordered from top to bottom
+     */
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return new LinkedList<>();
+        }
+
+        List<Integer> out = new LinkedList<>();
+        dfs(root, out, 0);
+        return out;
+    }
+
+    /**
+     * DFS traversal.
+     *
+     * @param r     root node
+     * @param out   output list
+     * @param level # of largest reached level
+     */
+    private void dfs(TreeNode r, List<Integer> out, int level) {
+        if (r == null) {
+            return;
+        }
+
+        if (level == out.size()) {
+            out.add(r.val);
+        }
+
+        dfs(r.right, out, level + 1);
+        dfs(r.left, out, level + 1);
+    }
+
+    /**
+     * Another basic solution based on BFS.
+     * Each time, add all nodes in same level and add the rightmost one to the output list.
+     *
+     * @param root given root node
+     * @return values of the nodes you can see ordered from top to bottom
+     */
+    public List<Integer> bfs(TreeNode root) {
+        if (root == null) {
+            return new LinkedList<>();
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        List<Integer> out = new LinkedList<>();
+        int size;
+
+        while (!q.isEmpty()) {
+            size = q.size();
+            TreeNode tmp = null;
+
+            for (int i = 0; i < size; i++) {
+                tmp = q.poll();
+                if (tmp.left != null) {
+                    q.add(tmp.left);
+                }
+                if (tmp.right != null) {
+                    q.add(tmp.right);
+                }
+            }
+
+            out.add(tmp.val);       // add rightmost node value
+        }
+
+        return out;
+    }
+}
