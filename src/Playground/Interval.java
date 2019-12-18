47849edac2d35200721eb1e7a4c8949a59ce2693
Null <borismirage00@gmail.com>
2019-12-18 13:04:07 -0800

    feat: interval for 759. Employee Free Time

---
+++ src/Playground/Interval.java
@@ -0,0 +1,31 @@
+package Playground;
+
+/**
+ * Definition for an Interval.
+ *
+ * @author BorisMirage
+ * Time: 2019/12/02 18:57
+ * Created with IntelliJ IDEA
+ */
+class Interval {
+    public int start;
+    public int end;
+
+    /**
+     * Constructor of interval.
+     */
+    public Interval() {
+    }
+
+    /**
+     * Constructor of new interval.
+     *
+     * @param _start start of interval
+     * @param _end   end of interval
+     */
+    public Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+}
+
