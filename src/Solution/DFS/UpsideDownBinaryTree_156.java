fc77e2d72fefe8fd2f2e09a4927bb274004c3b4e
Null <borismirage00@gmail.com>
2019-12-26 17:45:22 -0800

    feat: add iterative solution

--- src/Solution/DFS/UpsideDownBinaryTree_156.java
+++ src/Solution/DFS/UpsideDownBinaryTree_156.java
@@ -17,7 +17,7 @@ public class UpsideDownBinaryTree_156 {
      * "Upside down" first is to turn previous left most child to new root node.
      * Then move previous root to its left child's right node, and move root's left child to new root's left.
      *
-     * @param root root node
+     * @param root root node of given tree
      * @return flipped tree
      */
     public TreeNode upsideDownBinaryTree(TreeNode root) {
@@ -29,7 +29,8 @@ public class UpsideDownBinaryTree_156 {
 
         TreeNode newRoot = upsideDownBinaryTree(root.left);     // return left most child node
 
-        /* "Upside down" first is to turn previous left most child to new root node.
+        /*
+         * "Upside down" first is to turn previous left most child to new root node.
          * Then move previous root to its left child's right node, and move root's left child to new root's left. */
         root.left.left = root.right;
         root.left.right = root;
@@ -38,5 +39,30 @@ public class UpsideDownBinaryTree_156 {
         root.right = null;
 
         return newRoot;     // return current level root for next recursion
+    }
+
+    /**
+     * Iterative solution.
+     * For each node, this "upside down" operates same.
+     * Convert left child of current node to its parent, and current node becomes left child's right child.
+     * Current node's right child becomes left child's (now converted to current node's parent) left child.
+     * Note that in this problem, right child will either be leave node, or empty.
+     *
+     * @param root root node of given tree
+     * @return flipped tree
+     */
+    public TreeNode iterative(TreeNode root) {
+        TreeNode current = root, previous = null, tmp = null, next;
+
+        while (current != null) {
+            next = current.left;        // store next node first
+            current.left = tmp;         // left child is previous node's right child, initially, it is null
+            tmp = current.right;        // save next tmp node
+            current.right = previous;   // current node's right child is previous node
+            previous = current;         // make previous node to current node
+            current = next;             // move current node to next
+        }
+
+        return previous;
     }
 }
