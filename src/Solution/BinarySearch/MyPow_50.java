b4ff342614f0e47aab7b9f50acaaca8f2817e87b
Null <borismirage00@gmail.com>
2020-02-04 11:47:05 -0800

    docs: add description

--- src/Solution/BinarySearch/MyPow_50.java
+++ src/Solution/BinarySearch/MyPow_50.java
@@ -1,6 +1,8 @@
-package Solution.Math;
+package Solution.BinarySearch;
 
 /**
+ * Implement pow(x, n), which calculates x raised to the power n (x^n).
+ *
  * @author BorisMirage
  * Time: 2018/06/26 20:53
  * Created with IntelliJ IDEA
@@ -8,12 +10,11 @@ package Solution.Math;
 
 public class MyPow_50 {
     /**
-     * Implement pow(x, n), which calculates x raised to the power n (x^n).
-     *
-     * x^2n = x^n^2 = x^n * x^n
+     * x^(2n) = x^n^2 = x^n * x^n
+     * Binary search.
      *
      * @param x input base double
-     * @param n power
+     * @param n exponent
      * @return Returns a double containing the results of the power operation.
      */
 
@@ -25,24 +26,24 @@ public class MyPow_50 {
         }
 
         long pow = n;
-        if (pow < 0) {
+
+        if (pow < 0) {      // if the exponent is negative, then base should be converted
             x = 1 / x;
             pow = -pow;
         }
+
         double res = 1;
         double currentProduct = x;
 
-
         for (long i = pow; i > 0; i /= 2) {
 
-            /* Current pow is odd */
-            if (i % 2 == 1) {
+            if (i % 2 == 1) {       // current pow is odd
                 res = res * currentProduct;
             }
 
-            /* Current pow is even */
-            currentProduct = currentProduct * currentProduct;
+            currentProduct = currentProduct * currentProduct;       // current pow is even
         }
+
         return res;
     }
 }
