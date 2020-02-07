7392fe87a72e90b24d8894c44288f6e488399762
Null <borismirage00@gmail.com>
2020-02-07 11:31:31 -0800

    feat: add parent

--- src/Lib/Tree/Node.java
+++ src/Lib/Tree/Node.java
@@ -1,4 +1,4 @@
-package Lib;
+package Lib.Tree;
 
 /**
  * @author BorisMirage
@@ -11,14 +11,15 @@ public class Node {
     public Node left;
     public Node right;
     public Node next;
+    public Node parent;
 
     public Node() {
     }
 
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
+    public Node(int val, Node left, Node right, Node next, Node parent) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+        this.next = next;
     }
 }
