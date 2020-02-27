3513cb32a6213a34944d1b73ad7cd38b3cd391ac
Null <borismirage00@gmail.com>
2020-02-27 10:03:49 -0800

    docs: minor revision

--- src/Solution/Stack/RemoveDuplicateLetters_316.java
+++ src/Solution/Stack/RemoveDuplicateLetters_316.java
@@ -15,12 +15,15 @@ import java.util.LinkedList;
 
 public class RemoveDuplicateLetters_316 {
     /**
-     * First, iterate the array to count each char's appearance.
-     * Then iterate this array again.
-     * If found a char has been in stack, pass this char. Delete this char to keep in smallest lexicographical order.
-     * If found a char that is smaller than top of stack, pop it if this top char has more than one appearance in array.
-     * The purpose of this process is to assure output result is in smallest lexicographical order.
-     * Stack is not to store temporary char during first iteration, it is to store final output result.
+     * The output string should follow these two rules:
+     * 1. No duplicated character.
+     * 2. The smaller character in lexicographical order should be removed later, if it contains duplicated.
+     * The solution is using stack or deque.
+     * First, iterate the array to count each char's appearance. Then iterate this array again.
+     * If found a char in stack, pass this char, and in this way to remove all duplicated character.
+     * Otherwise, pop out all characters that is larger than current character AND contains duplicate.
+     * Use a boolean array to record duplicated character. When pop out character, mark popped character as unvisited.
+     * Add current character to stack and continue this process until the whole string is traversed.
      *
      * @param s given string
      * @return string without duplicated letter and be smallest in lexicographical order
@@ -44,15 +47,16 @@ public class RemoveDuplicateLetters_316 {
         for (char c : arr) {
             count[c - 'a']--;
 
-            if (!isVisited[c - 'a']) {      // pass chars already in stack and in this way to delete duplicated char
+            if (!isVisited[c - 'a']) {      // pass chars already in stack and delete duplicated char
 
                 /*
                  * Pop out elements that:
                  * 1. Larger than current char (to assure result smallest lexicographical order)
                  * 2. This popped char will be found in later of string (count[c] > 0)
-                 * Both conditions should be met. */
+                 * Both conditions should be met.
+                 * Pop out all characters that meet conditions above. They will be added in to stack later. */
                 while (!dq.isEmpty() && dq.peekLast() > c && count[dq.peekLast() - 'a'] > 0) {
-                    isVisited[dq.pollLast() - 'a'] = false;     // popped char may be seen in later of array
+                    isVisited[dq.pollLast() - 'a'] = false;     // reset visited mark, so that they will be added later
                 }
                 dq.addLast(c);
                 isVisited[c - 'a'] = true;
@@ -60,10 +64,14 @@ public class RemoveDuplicateLetters_316 {
         }
 
         StringBuilder sb = new StringBuilder();
-
         while (!dq.isEmpty()) {
             sb.append(dq.pollFirst());
         }
+
         return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new RemoveDuplicateLetters_316().removeDuplicateLetters("cbaacdcbc"));        // acdb
     }
 }
