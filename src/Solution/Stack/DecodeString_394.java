1d34ca9665b545c78d2587030f4a8150cade939d
Null <borismirage00@gmail.com>
2020-03-01 10:26:14 -0800

    style: minor revision

--- src/Solution/Stack/DecodeString_394.java
+++ src/Solution/Stack/DecodeString_394.java
@@ -23,49 +23,46 @@ public class DecodeString_394 {
      */
     public String decodeString(String s) {
 
-        Stack<Integer> repeat = new Stack<>();      // save repeat time
-        Stack<String> temp = new Stack<>();     // save temporary string
+        /* Corner case */
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+
+        Stack<Integer> repeat = new Stack<>();      // count of repeating string clip
+        Stack<String> tmp = new Stack<>();          // temporary string clip
+
         String out = "";
         int i = 0;
 
         while (i < s.length()) {
             if (Character.isDigit(s.charAt(i))) {
-                int count = 0;
+                int digit = 0;
                 while (Character.isDigit(s.charAt(i))) {
-                    count = 10 * count + (s.charAt(i) - '0');
-                    i++;
+                    digit = 10 * digit + (s.charAt(i++) - '0');      // convert char to digit
                 }
-                repeat.push(count);     // add repeat time to stack
+                repeat.push(digit);
             } else if (s.charAt(i) == '[') {
-                temp.push(out);     // add previous decoded string to stack for later repeat process
-                out = "";       // reset to record next string clip
+                tmp.push(out);      // string clip must comes after repeat count
+                out = "";
                 i++;
             } else if (s.charAt(i) == ']') {
-
-                /*
-                 * Top of the stack is the substring before repeat count.
-                 * Add this part to StringBuilder and then add repeat part to the end of it. */
-                StringBuilder cache = new StringBuilder(temp.pop());
+                StringBuilder sb = new StringBuilder(tmp.pop());     // add previous string clip to the beginning
                 int count = repeat.pop();
-                for (int j = 0; j < count; j++) {
-                    cache.append(out);      // concatenate repeat substring to previous non-repeat substring
+                while (count-- > 0) {
+                    sb.append(out);     // add repeating string clip
                 }
-
-                /*
-                 * Now this string will have two possibility:
-                 * 1. Becomes the repetition of previous part, and will be reset after repetition is completed.
-                 * 2. Be reset and push to stack when a new '[' is found. */
-                out = cache.toString();
+                out = sb.toString();
                 i++;
             } else {
-                out += s.charAt(i++);       // if current char is normal alphabet, continue add it to the end of output
+                out += s.charAt(i++);       // adding character to the end of current string clip
             }
         }
+
         return out;
     }
 
     public static void main(String[] args) {
         DecodeString_394 test = new DecodeString_394();
-        System.out.println(test.decodeString("3[a2[c]]"));
+        System.out.println(test.decodeString("3[a2[c]]"));      // accaccacc
     }
 }
