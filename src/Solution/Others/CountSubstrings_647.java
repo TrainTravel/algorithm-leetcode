5a18d5413130d4bbf290443e9e94fb421af80777
Null <borismirage00@gmail.com>
2020-03-05 11:06:24 -0800

    solution: DP & Manacher

--- src/Solution/Others/CountSubstrings_647.java
+++ src/Solution/Others/CountSubstrings_647.java
@@ -52,4 +52,84 @@ public class CountSubstrings_647 {
 
         return count;
     }
+
+    /**
+     * Almost same as finding longest palindromic substrings.
+     * The difference is that, if found a palindromic substring, then add 1 to result.
+     *
+     * @param s given string
+     * @return count number of palindromic substrings in this string
+     */
+    public int countSubstringsDP(String s) {
+
+        /* Corner case */
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int n = s.length(), count = 1;
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && ((i - j) <= 2 || dp[i - 1][j + 1]);
+                if (dp[i][j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Manacher algorithm. The idea is the same.
+     * First of all, preprocess string into a array. For instance, cbcbc -> ^#c#b#c#b#c#$.
+     * Then use a array to count length of longest palindromic substring at center of each char.
+     * Traverse the array, count the center of longest palindromic substring (C) and its radius (R).
+     * After finding all number of palindromic substrings, traverse the int array again.
+     * If current char has a palindromic substring with length of n, then the total # will be n / 2.
+     *
+     * @param s given string
+     * @return count number of palindromic substrings in this string
+     */
+    public int countSubstringsManacher(String s) {
+
+        /* Corner case */
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] array = new char[2 * s.length() + 3];        // + 3 is to allow special beginning and end
+        array[0] = '@';
+        array[1] = '#';
+        array[array.length - 1] = '$';
+        int index = 2;
+        for (char c : s.toCharArray()) {        // preprocess string
+            array[index++] = c;
+            array[index++] = '#';
+        }
+
+        int[] palindromicLength = new int[array.length];
+        int center = 0, right = 0;
+        for (int i = 1; i < palindromicLength.length - 1; ++i) {
+            if (i < right) {
+                palindromicLength[i] = Math.min(right - i, palindromicLength[2 * center - i]);
+            }
+            while (array[i + palindromicLength[i] + 1] == array[i - palindromicLength[i] - 1]) {
+                palindromicLength[i]++;
+            }
+            if (i + palindromicLength[i] > right) {
+                center = i;
+                right = i + palindromicLength[i];
+            }
+        }
+
+        int count = 0;
+        for (int c : palindromicLength) {       // count number of palindromic substrings in s
+            count += (c + 1) / 2;               // each palindromic substrings contains n / 2 palindromic substrings
+        }
+
+        return count;
+    }
 }
