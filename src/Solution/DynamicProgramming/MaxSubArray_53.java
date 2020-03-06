6125bbcd74f8feb961c3371cb0ed98b22936d944
Null <borismirage00@gmail.com>
2020-03-05 17:36:40 -0800

    solution: one-pass traverse

--- src/Solution/DynamicProgramming/MaxSubArray_53.java
+++ src/Solution/DynamicProgramming/MaxSubArray_53.java
@@ -1,6 +1,5 @@
 package Solution.DynamicProgramming;
 
-
 /**
  * Given an integer array nums
  * Find the contiguous sub array (containing at least one number) which has the largest sum and return its sum.
@@ -12,34 +11,30 @@ package Solution.DynamicProgramming;
 
 public class MaxSubArray_53 {
     /**
-     * Basic dynamic programming.
-     * dp[i] = dp[i - 1] + nums[i], if dp[i - 1] + dp[i] > 0
-     * dp[i] = nums[i], otherwise
-     * max = Math.max(dp[i], max)
+     * One-pass traverse array. Use an int to store the prefix sum in array.
+     * If prefix sum is less than 0, reset sum to 0.
+     * Compare prefix sum to max subarray sum, and select the larger one during the traverse.
      *
      * @param nums input int array
      * @return max contiguous sub array sum
      */
     public int maxSubArray(int[] nums) {
 
-        /* Corner case*/
-        if (nums.length == 0) {
+        /* Corner case */
+        if (nums == null || nums.length == 0) {
             return 0;
         }
 
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        int max = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE, current = 0;
 
-        for (int i = 1; i < dp.length; i++) {
-            if (dp[i - 1] + dp[i] > 0) {
-                dp[i] = dp[i - 1] + nums[i];
-            } else {
-                dp[i] = nums[i];
+        for (int n : nums) {
+            current += n;
+            max = Math.max(max, current);
+            if (current < 0) {
+                current = 0;
             }
-            max = Math.max(dp[i], max);
-
         }
+
         return max;
     }
 }
