043a496887daaa7c92bccd23970b2a9f78957507
Null <borismirage00@gmail.com>
2020-03-14 18:00:18 -0700

    solution: add heap solution

--- src/Solution/Array/LargestNumber_179.java
+++ src/Solution/Array/LargestNumber_179.java
@@ -2,6 +2,7 @@ package Solution.Array;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Given a list of non negative integers, arrange them such that they form the largest number.
@@ -10,7 +11,6 @@ import java.util.Comparator;
  * Time: 2019/07/06 15:56
  * Created with IntelliJ IDEA
  */
-
 public class LargestNumber_179 {
     /**
      * The principle of comparison is similar to dictionary lexical order.
@@ -18,9 +18,45 @@ public class LargestNumber_179 {
      * If two numbers have same first digit, then compare second digit with same rule.
      *
      * @param nums given array
-     * @return largest # that this array could make up
+     * @return largest number that this array could make up
      */
     public String largestNumber(int[] nums) {
+
+        /* Corner case */
+        if (nums == null || nums.length == 0) {
+            return "";
+        }
+
+        PriorityQueue<String> pq = new PriorityQueue<>((o1, o2) -> {
+            String s1 = o1 + o2;
+            String s2 = o2 + o1;
+            return s2.compareTo(s1);
+        });
+
+        for (int n : nums) {
+            pq.add(String.valueOf(n));      // convert int to string
+        }
+
+        if (!pq.isEmpty() && pq.peek().charAt(0) == '0') {      // if whole array is 0
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!pq.isEmpty()) {
+            sb.append(pq.poll());
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Idea is same, instead using comparator to sort the array instead of use heap.
+     *
+     * @param nums given array
+     * @return largest number that this array could make up
+     */
+    public String largestNumberComparator(int[] nums) {
 
         /* Corner case */
         if (nums == null || nums.length == 0) {
