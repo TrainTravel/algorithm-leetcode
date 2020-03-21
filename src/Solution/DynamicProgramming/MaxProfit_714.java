686a5c4e7759e2ebf67e887c65569cfc30d888d4
Null <borismirage00@gmail.com>
2020-03-21 13:09:18 -0700

    docs: add description

--- src/Solution/DynamicProgramming/MaxProfit_714.java
+++ src/Solution/DynamicProgramming/MaxProfit_714.java
@@ -1,6 +1,12 @@
 package Solution.DynamicProgramming;
 
 /**
+ * Your are given an array of integers prices, for which the i-th element is the price of a given stock on day i.
+ * There is a non-negative integer fee representing a transaction fee.
+ * You may complete as many transactions as you like. But you need to pay the transaction fee for each transaction.
+ * You may not buy more than 1 share of a stock at a time (ie. you must sell the stock share before you buy again.)
+ * Return the maximum profit you can make.
+ *
  * @author BorisMirage
  * Time: 2020/03/21 11:00
  * Created with IntelliJ IDEA
