1beae35e334185e5ab982d665bd3ae28530ac3c9
Null <borismirage00@gmail.com>
2020-03-28 09:45:16 -0700

    style: minor revision

--- src/Solution/Greedy/IsSubsequence_392.java
+++ src/Solution/Greedy/IsSubsequence_392.java
@@ -59,24 +59,27 @@ public class IsSubsequence_392 {
      */
     public boolean bucket(String s, String t) {
 
-        List<Integer>[] idx = new List[256];
+        List<Integer>[] index = new List[256];
         for (int i = 0; i < t.length(); i++) {
-            if (idx[t.charAt(i)] == null)
-                idx[t.charAt(i)] = new ArrayList<>();
-            idx[t.charAt(i)].add(i);
+            if (index[t.charAt(i)] == null) {
+                index[t.charAt(i)] = new ArrayList<>();
+            }
+            index[t.charAt(i)].add(i);
         }
 
-        int prev = 0;
+        int previous = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (idx[s.charAt(i)] == null) {
+            if (index[s.charAt(i)] == null) {
                 return false;
             }
-            int j = Collections.binarySearch(idx[s.charAt(i)], prev);
-            if (j < 0) j = -j - 1;
-            if (j == idx[s.charAt(i)].size()) {
+            int j = Collections.binarySearch(index[s.charAt(i)], previous);     // binary search to find insert position
+            if (j < 0) {
+                j = -j - 1;
+            }
+            if (j == index[s.charAt(i)].size()) {
                 return false;
             }
-            prev = idx[s.charAt(i)].get(j) + 1;
+            previous = index[s.charAt(i)].get(j) + 1;
         }
 
         return true;
