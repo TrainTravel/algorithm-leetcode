bc49d6755823fdb5e0129b8906d1b8bc559fec2e
Null <borismirage00@gmail.com>
2020-04-06 13:46:09 -0700

    docs: minor revision

--- src/Solution/Heap/MedianFinder_295.java
+++ src/Solution/Heap/MedianFinder_295.java
@@ -4,6 +4,15 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
+ * Median is the middle value in an ordered integer list.
+ * If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+ * For example,
+ * [2,3,4], the median is 3
+ * [2,3], the median is (2 + 3) / 2 = 2.5
+ * Design a data structure that supports the following two operations:
+ * 1. void addNum(int num) - Add a integer number from the data stream to the data structure.
+ * 2. double findMedian() - Return the median of all elements so far.
+ *
  * @author BorisMirage
  * Time: 2020/02/20 15:22
  * Created with IntelliJ IDEA
@@ -12,11 +21,16 @@ import java.util.Queue;
 public class MedianFinder_295 {
     private Queue<Integer> small;
     private Queue<Integer> large;
-    private boolean even = true;
+    private boolean even = true;        // initially, the size of 2 heaps are both 0, which is even
 
     /**
-     * Use two heaps.
-     * Max heap stores smaller part of stream, min heap stores larger part of stream.
+     * Keeping two heaps. One min heap stores larger part of stream and one max heap stores smaller part.
+     * The size of two heaps should be same, or the large heap can have one more elements when total size is odd.
+     * There are two conditions:
+     * 1. If two heaps have same size, then add one element to large heap.
+     * 2. If total size is odd, then add one element to small heap.
+     * The adding process is, for example, add one element to large heap:
+     * Add given element to small heap first, then poll out the top of small heap, add the polled element to large heap.
      */
     public MedianFinder_295() {
         small = new PriorityQueue<>((o1, o2) -> o2 - o1);               // max heap stores smaller part of stream
@@ -35,12 +49,12 @@ public class MedianFinder_295 {
      * @param num given integer
      */
     public void addNum(int num) {
-        if (even) {     // if total size is even, then add value to large heap
-            large.offer(num);
-            small.offer(large.poll());      // move top of large heap (smallest in larger part of stream) to small heap
+        if (even) {
+            small.add(num);
+            large.add(small.poll());
         } else {
-            small.offer(num);
-            large.offer(small.poll());      // move top of small heap to large heap
+            large.add(num);
+            small.add(large.poll());
         }
 
         even = !even;
@@ -57,7 +71,7 @@ public class MedianFinder_295 {
         if (even) {
             return (small.peek() + large.peek()) / 2.0;
         } else {
-            return (double) small.peek();       // if total length is odd, then return top of small heap
+            return (double) large.peek();       // if total length is odd, then return top of small heap
         }
     }
 }
